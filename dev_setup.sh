0adde310ed59fe3779cba8946b9e2e213926dd52
Sean Fitzgerald <sean@fitzgeralds.me>
2016-05-23 22:47:00 -0700

    Updating scripts and docs for dev environment setup.

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,6 +1,36 @@
 #!/usr/bin/env bash
+######################################################
+# dev_setup.sh
+# @author sean.fitzgerald (aka clusterfudge)
+#
+# The purpose of this script is to create a self-
+# contained development environment using
+# virtualenv for python dependency sandboxing.
+# This script will create a virtualenv (using the
+# conventions set by virtualenv-wrapper for
+# location and naming) and install the requirements
+# laid out in requirements.txt, pocketsphinx, and
+# pygtk into the virtualenv. Mimic will be
+# installed and built from source inside the local
+# checkout.
+#
+# The goal of this script is to create a development
+# environment in user space that is fully functional.
+# It is expected (and even encouraged) for a developer
+# to work on multiple projects concurrently, and a
+# good OSS citizen respects that and does not pollute
+# a developers workspace with it's own dependencies
+# (as much as possible).
+# </endRant>
+######################################################
+
 # exit on any error
 set -Ee
+
+if [ $(id -u) -eq 0 ]; then
+  echo "This script should not be run as root or with sudo."
+  exit 1
+fi
 
 TOP=$(cd $(dirname $0) && pwd -L)
 VIRTUALENV_ROOT=${VIRTUALENV_ROOT:-"${HOME}/.virtualenvs/mycroft"}
