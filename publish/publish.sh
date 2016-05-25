82285eaa6648c3c02c7bf4504dd754e2c493a311
Arron Atchison <arron.atchison@gmail.com>
2016-05-25 10:05:40 -0500

    Resurrecting attempts to move the bootstrap process into legitimate deb packages

--- publish/publish.sh
+++ publish/publish.sh
@@ -83,6 +83,30 @@ replace ${CONTROL_FILE} "%%PACKAGE%%" "mycroft-standalone"
 replace ${CONTROL_FILE} "%%VERSION%%" "${VERSION}"
 replace ${CONTROL_FILE} "%%ARCHITECTURE%%" "${ARCH}"
 replace ${CONTROL_FILE} "%%DESCRIPTION%%" "mycroft-standalone"
+replace ${CONTROL_FILE} "%%PRE_DEPENDS%%" "portaudio19-dev, libglib2.0-0, flac, espeak, mpg123"
+echo "Creating debian preinst file"
+PREINST_FILE=${DEB_DIR}/DEBIAN/preinst
+cp ${TOP}/publish/deb_base/preinst.template ${PREINST_FILE}
+replace ${PREINST_FILE} "%%INSTALL_USER%%" "mycroft"
+chmod 0755 ${PREINST_FILE}
+
+echo "Creating debian postinst file"
+POSTINST_FILE=${DEB_DIR}/DEBIAN/postinst
+cp ${TOP}/publish/deb_base/postinst.template ${POSTINST_FILE}
+replace ${POSTINST_FILE} "%%INSTALL_USER%%" "mycroft"
+chmod 0755 ${POSTINST_FILE}
+
+echo "Creating debian prerm file"
+PRERM_FILE=${DEB_DIR}/DEBIAN/prerm
+cp ${TOP}/publish/deb_base/prerm.template ${PRERM_FILE}
+#replace ${PRERM_FILE} "%%INSTALL_USER%%" "mycroft"
+chmod 0755 ${PRERM_FILE}
+
+echo "Creating debian postrm file"
+POSTRM_FILE=${DEB_DIR}/DEBIAN/postrm
+cp ${TOP}/publish/deb_base/postrm.template ${POSTRM_FILE}
+replace ${POSTRM_FILE} "%%INSTALL_USER%%" "mycroft"
+chmod 0755 ${POSTRM_FILE}
 
 # setup init scripts
 function setup_init_script() {
