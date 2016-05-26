40b5cf0da9282450a133bf9dd017067edc46f60a
Ryan Sipes <ryan.sipes@mycroft.ai>
2016-05-26 11:41:40 -0500

    Formatting Fix in Pairing Skill
    Blank line at end of file removed.

--- mycroft/skills/pairing/__init__.py
+++ mycroft/skills/pairing/__init__.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
 
+
 import threading
 
 from adapt.intent import IntentBuilder
