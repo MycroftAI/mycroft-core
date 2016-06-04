bbeabead11ad22379b1fd79c8784f079dbd9edb1
Eugene R. Miller <theerm@gmail.com>
2016-06-04 12:59:30 -0600

    Added hello world

---
+++ mycroft/skills/hello_world/__init__.py
@@ -0,0 +1,98 @@
+# Copyright 2016 Mycroft AI, Inc.
+#
+# This file is part of Mycroft Core.
+#
+# Mycroft Core is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Mycroft Core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
+
+from os.path import dirname
+
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+from random import randint
+from pprint import pformat
+from subprocess import check_output
+
+__author__ = 'the7erm'
+
+LOGGER = getLogger(__name__)
+
+# This skill is based on the welcome skill.
+# The goal of this skill is to give a new developer a basic skill with
+# comments.
+
+class HelloSkill(MycroftSkill):
+
+    def __init__(self):
+        super(HelloSkill, self).__init__(name="HelloSkill")
+
+    def initialize(self):
+        # Data file location: `mycroft/skills/hello_world/`
+        self.load_data_files(dirname(__file__))
+
+
+        # Read this: https://github.com/MycroftAI/adapt#intent-modelling
+        hello_intent = IntentBuilder("HelloIntent")
+        # Add a .voc file.
+        # The .voc file is the list of words that your intent will accept
+        # as input.
+        # HellowKeyword tells the mycroft to use
+        # `hello_world/vocab/<lang>/HelloKeyword.voc`
+        hello_intent.require("HelloKeyword")
+        hello_intent.build()
+        """
+            # You can also chain your intent.
+            hello_intent = IntentBuilder("HelloIntent")\
+                .require("HelloKey")\
+                .build()
+        """
+
+        self.register_intent(hello_intent, self.handle_hello_intent)
+
+    def handle_hello_intent(self, message):
+        # Sometimes it's useful to log what you're doing while debugging.
+        LOGGER.debug("called handle_hello_intent message.metadata:%s" %
+                     message.metadata)
+
+        # At this point most people would this line and call it good
+        # for a hello_world example, but let's have some fun and mix it up.
+        # self.speak_dialog('Hello')
+
+        dialogs = [
+            'Hello', # `hello_world/dialog/<lang>/Hello.dialog`
+            'Hi' # `hello_world/dialog/<lang>/Hi.dialog`
+        ]
+        idx = randint(0, len(dialogs) - 1)
+        dialog = dialogs[idx]
+        self.speak_dialog(dialog)
+        LOGGER.debug("Said: '%s'" % dialog)
+
+        if message.metadata['utterance'] == "how are you doing today":
+            self.speak_dialog("Fine") # `hello_world/dialog/<lang>/Fine.dialog`
+            self.speak_dialog("WorkingHardOn") # `hello_world/dialog/<lang>
+                                               #  /WorkingHardOn.dialog`
+
+            # Get the 4 highest CPU processes.
+            output = check_output("ps -eo pcpu,comm --no-headers|"
+                                  "sort -t. -nk1,2 -k4,4 -r |"
+                                  "head -n 4 |"
+                                  "awk '{print $2}'", shell=True)
+            self.speak(output.replace("\n", ", "))
+
+    def stop(self):
+        pass
+
+
+def create_skill():
+    return HelloSkill()
