89234c1629dbfee697c6975884401b3195f6231d
Arron Atchison <arron.atchison@gmail.com>
2016-06-06 13:49:07 -0500

    removed mimic from dev_setup

--- dev_setup.sh
+++ dev_setup.sh
@@ -66,8 +66,8 @@ cd ${TOP}/pocketsphinx-python
 python setup.py install
 
 #build and install mimic
-cd ${TOP}
-${TOP}/install-mimic.sh
+#cd ${TOP}
+#${TOP}/install-mimic.sh
 
 # install pygtk for desktop_launcher skill
 ${TOP}/install-pygtk.sh
--- publish/publish.sh
+++ publish/publish.sh
@@ -162,7 +162,7 @@ mv *.deb ${TOP}/dist
 
 cd ${TOP}/dist
 _run s3cmd -c ${HOME}/.s3cfg.mycroft-artifact-writer sync --acl-public . s3://bootstrap.mycroft.ai/artifacts/apt/${ARCH}/${VERSION}/
-echo ${VERSION} > ${TOP}/dist/latest
+echo ${VERSION} > latest
 _run s3cmd -c ${HOME}/.s3cfg.mycroft-artifact-writer put --acl-public ${TOP}/dist/latest s3://bootstrap.mycroft.ai/artifacts/apt/${ARCH}/latest #cd ${TOP}/dist
 
 
