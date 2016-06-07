6cfe4a765c50c408a082a395b8786bbcc5bf4ad5
Jonathan D'Orleans <jonathan.dorleans@mycroft.ai>
2016-06-06 23:17:13 -0400

    Issues 96 - Configuration Test - Adding AbstractConfigurationTest - Adding ConfigurationManagerTest

--- test/configuration/__init__.py
+++ test/configuration/__init__.py
@@ -1,11 +1,11 @@
 import unittest
 
-from mycroft.configuration import ConfigurationLoader
+from mycroft.configuration import ConfigurationLoader, ConfigurationManager
 
 __author__ = 'jdorleans'
 
 
-class ConfigurationLoaderTest(unittest.TestCase):
+class AbstractConfigurationTest(unittest.TestCase):
     @staticmethod
     def create_config(lang='en-us', module='mimic'):
         config = {
@@ -26,21 +26,23 @@ class ConfigurationLoaderTest(unittest.TestCase):
         mod = tts.get('module', None)
         self.assertEquals(mod, module)
 
-    def test_load_default(self):
+
+class ConfigurationLoaderTest(AbstractConfigurationTest):
+    def test_load(self):
         config = ConfigurationLoader.load()
         self.assert_config(config)
 
-    def test_load_override_custom(self):
+    def test_load_and_override_custom(self):
         config = self.create_config('pt-br', 'espeak')
         config = ConfigurationLoader.load(config)
         self.assert_config(config)
 
-    def test_load_override_default(self):
+    def test_load_and_override_default(self):
         config = self.create_config()
         config = ConfigurationLoader.load(config, ['./mycroft.ini'])
         self.assert_config(config, 'pt-br', 'espeak')
 
-    def test_load_do_not_override(self):
+    def test_load_and_do_not_override_custom(self):
         my_config = {'key': 'value'}
         config = ConfigurationLoader.load(my_config)
         self.assert_config(config)
@@ -49,75 +51,26 @@ class ConfigurationLoaderTest(unittest.TestCase):
         self.assertIsNotNone(value)
         self.assertEquals(value, my_config.get('key'))
 
-    def test_load_invalid_config_type(self):
+    def test_load_with_invalid_config_type(self):
         invalid_type = 'invalid_type'
         config = ConfigurationLoader.load(invalid_type)
         self.assertEquals(config, invalid_type)
 
-    def test_load_invalid_locations_type(self):
+    def test_load_with_invalid_locations_type(self):
         config = ConfigurationLoader.load(None, './mycroft.ini')
         self.assertEquals(config, {})
 
-    def test_load_invalid_locations_path(self):
+    def test_load_with_invalid_locations_path(self):
         locations = ['./invalid/mycroft.ini', './invalid_mycroft.ini']
         config = ConfigurationLoader.load(None, locations)
         self.assertEquals(config, {})
 
 
-class ConfigurationManagerTest(unittest.TestCase):
-    @staticmethod
-    def create_config(lang='en-us', module='mimic'):
-        config = {
-            'core': {'lang': lang},
-            'tts': {'module': module}
-        }
-        return config
-
-    def assert_config(self, config, lang='en-us', module='mimic'):
-        self.assertIsNotNone(config)
-        core = config.get('core', None)
-        self.assertIsNotNone(core)
-        lan = core.get('lang', None)
-        self.assertIsNotNone(lan)
-        self.assertEquals(lan, lang)
-        tts = config.get('tts', None)
-        self.assertIsNotNone(tts)
-        mod = tts.get('module', None)
-        self.assertEquals(mod, module)
-
-    def test_load_default(self):
-        config = ConfigurationLoader.load()
+class ConfigurationManagerTest(AbstractConfigurationTest):
+    def test_load_defaults(self):
+        config = ConfigurationManager.load_defaults()
         self.assert_config(config)
 
-    def test_load_override_custom(self):
-        config = self.create_config('pt-br', 'espeak')
-        config = ConfigurationLoader.load(config)
+    def test_load_local(self):
+        config = ConfigurationManager.load_local()
         self.assert_config(config)
-
-    def test_load_override_default(self):
-        config = self.create_config()
-        config = ConfigurationLoader.load(config, ['./mycroft.ini'])
-        self.assert_config(config, 'pt-br', 'espeak')
-
-    def test_load_do_not_override(self):
-        my_config = {'key': 'value'}
-        config = ConfigurationLoader.load(my_config)
-        self.assert_config(config)
-
-        value = config.get('key', None)
-        self.assertIsNotNone(value)
-        self.assertEquals(value, my_config.get('key'))
-
-    def test_load_invalid_config_type(self):
-        invalid_type = 'invalid_type'
-        config = ConfigurationLoader.load(invalid_type)
-        self.assertEquals(config, invalid_type)
-
-    def test_load_invalid_locations_type(self):
-        config = ConfigurationLoader.load(None, './mycroft.ini')
-        self.assertEquals(config, {})
-
-    def test_load_invalid_locations_path(self):
-        locations = ['./invalid/mycroft.ini', './invalid_mycroft.ini']
-        config = ConfigurationLoader.load(None, locations)
-        self.assertEquals(config, {})
