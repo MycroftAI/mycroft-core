888a6372b0c066f69917d63146eddbfa380d8658
Ethan Ward <ethanandrewward@gmail.com>
2016-06-10 15:43:35 -0500

    Issues 63 - Refactor code to reduce size

--- test/skills/wolfram_alpha/__init__.py
+++ test/skills/wolfram_alpha/__init__.py
@@ -20,33 +20,31 @@ class WolframAlphaTest(unittest.TestCase):
         <pod title='" + pod_title + "'><subpod>\
         <plaintext>" + text + "</plaintext></subpod></pod></queryresult>"
 
+    def create_result(self, pod_title, value):
+        result = self.format_result(pod_title, value)
+        return wolframalpha.Result(StringIO(result))
+
     def test_result_pod(self):
-        result = self.format_result("Result", "7")
-        res = wolframalpha.Result(StringIO(result))
+        res = self.create_result("Result", "7")
         self.assertEquals(WolframAlphaSkill().get_result(res), "7")
 
     def test_value_pod(self):
-        result = self.format_result("Value", "2^3")
-        res = wolframalpha.Result(StringIO(result))
+        res = self.create_result("Value", "2^3")
         self.assertEquals(WolframAlphaSkill().get_result(res), "2^3")
 
     def test_notable_facts_pod(self):
-        result = self.format_result("NotableFacts:PeopleData", "PeopleData")
-        res = wolframalpha.Result(StringIO(result))
+        res = self.create_result("NotableFacts:PeopleData", "PeopleData")
         self.assertEquals(WolframAlphaSkill().get_result(res), "PeopleData")
 
     def test_basic_information_pod(self):
-        result = self.format_result("BasicInformation:PeopleData",
-                                    "Born in 1997")
-        res = wolframalpha.Result(StringIO(result))
+        res = self.create_result("BasicInformation:PeopleData",
+                                    "Born in 1997") 
         self.assertEquals(WolframAlphaSkill().get_result(res), "Born in 1997")
 
     def test_decimal_approximation_pod(self):
-        result = self.format_result("DecimalApproximation", "5.6666666666")
-        res = wolframalpha.Result(StringIO(result))
+        res = self.create_result("DecimalApproximation", "5.6666666666")
         self.assertEquals(WolframAlphaSkill().get_result(res), "5.666")
 
     def test_invalid_pod(self):
-        result = self.format_result("InvalidTitle", "Test")
-        res = wolframalpha.Result(StringIO(result))
+        res = self.create_result("InvalidTitle", "Test")
         self.assertEquals(WolframAlphaSkill().get_result(res), None)
