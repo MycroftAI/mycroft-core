3a9d3557c3b2dcb1664859510a21ba6b32b9f661
Jason Hines <jason@hinesnetwork.com>
2016-06-15 14:52:59 -0400

    Added optional stop to script per aatchison request

--- mycroft.sh
+++ mycroft.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-mkdir -p logs
-screen -mdS mycroft-service ./start.sh service >> logs/mycroft-service.log
-screen -mdS mycroft-skills ./start.sh skills >> logs/mycroft-skills.log
-screen -mdS mycroft-voice ./start.sh voice >> logs/mycroft-voice.log
+if [ ! -z $1 ]
+then
+     screen -XS mycroft-service quit
+     screen -XS mycroft-skills quit
+     screen -XS mycroft-voice quit
+     echo "Mycroft Stopped"
+
+else
+     mkdir -p logs
+     screen -mdS mycroft-service ./start.sh service >> logs/mycroft-service.log
+     screen -mdS mycroft-skills ./start.sh skills >> logs/mycroft-skills.log
+     screen -mdS mycroft-voice ./start.sh voice >> logs/mycroft-voice.log
+     echo "Mycroft Started\nTo stop Mycroft use 'mycroft.sh stop'"
+fi
+
