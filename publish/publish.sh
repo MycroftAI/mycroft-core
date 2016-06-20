2d4519d26459736f9ce076889ecc14bd6aa21b8e
aatchison <arron.atchison@gmail.com>
2016-06-20 17:31:41 -0500

    installing pocketsphinx from pip, 'casue it's radical

--- publish/publish.sh
+++ publish/publish.sh
@@ -136,8 +136,8 @@ mkdir -p ${DEB_DIR}/etc/mycroft
 cat > ${DEB_DIR}/etc/mycroft/mycroft.ini << EOM
 [tts]
 module = "mimic"
-mimic.path = "/opt/mycroft/bin/mimic"
-mimic.voice = "/opt/mycroft/lib/python2.7/site-packages/Mycroft-0.7.1-py2.7.egg/mycroft/tts/mycroft_voice_4.0.flitevox"
+mimic.path = "/usr/local/bin/mimic"
+mimic.voice = "/usr/local/lib/python2.7/site-packages/Mycroft-0.7.1-py2.7.egg/mycroft/tts/mycroft_voice_4.0.flitevox"
 
 [metrics_client]
 enabled = True
