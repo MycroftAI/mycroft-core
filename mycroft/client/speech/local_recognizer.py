437f1b904de1271591193f050b8837b8cc92e407
Matthew Scholefield <matthew3311999@gmail.com>
2016-06-21 09:25:47 -0500

    Fixed pocketsphinxnot loading and pep8 Previously I tried using a new word called 'hey-mycroft' (with the appropriate phoenetic version in the .dict) however it worked just as well as how it is now as separate words

--- mycroft/client/speech/local_recognizer.py
+++ mycroft/client/speech/local_recognizer.py
@@ -27,7 +27,8 @@ BASEDIR = os.path.dirname(os.path.abspath(__file__))
 
 
 class LocalRecognizer(object):
-    def __init__(self, sample_rate=16000, lang="en-us", key_phrase="hey-mycroft"):
+    def __init__(self, sample_rate=16000, lang="en-us",
+                 key_phrase="hey mycroft"):
         self.lang = lang
         self.key_phrase = key_phrase
         self.sample_rate = sample_rate
