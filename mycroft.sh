58bacc85cd7ee2ae56d7dfe09abcb3faa1612a9e
Jason Hines <jason@hinesnetwork.com>
2016-06-22 12:07:17 -0400

    Added code to get script directory so that the script can be run from any path

--- mycroft.sh
+++ mycroft.sh
@@ -1,3 +1,11 @@
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
 function usage {
   echo
   echo "Quickly start, stop or restart Mycroft's esential services in detached screens"
@@ -17,10 +25,10 @@ function usage {
 }
 
 function start-mycroft {
-  mkdir -p logs
-  screen -mdS mycroft-service -c mycroft-service.screen ./start.sh service
-  screen -mdS mycroft-skills -c mycroft-skills.screen ./start.sh skills
-  screen -mdS mycroft-voice -c mycroft-voice.screen ./start.sh voice
+  mkdir -p $DIR/logs
+  screen -mdS mycroft-service -c $DIR/mycroft-service.screen $DIR/start.sh service
+  screen -mdS mycroft-skills -c $DIR/mycroft-skills.screen $DIR/start.sh skills
+  screen -mdS mycroft-voice -c $DIR/mycroft-voice.screen $DIR/start.sh voice
 }
 function stop-mycroft {
   screen -XS mycroft-service quit
