1beed89e8faa5f4cc54f8181e9e2b6b0236e48c8
Ethan Ward <ethanandrewward@gmail.com>
2016-06-24 09:57:11 -0500

    Issues 158 - Address feedback

--- test/skills/scheduled_skills.py
+++ test/skills/scheduled_skills.py
@@ -10,20 +10,22 @@ logger = getLogger(__name__)
 
 
 class ScheduledSkillTest(unittest.TestCase):
+    skill = ScheduledSkill(name='ScheduledSkillTest')
+
     def test_formatted_time_today_hours(self):
         date = datetime.now() + timedelta(hours=2)
-        self.assertEquals(ScheduledSkill(name='test').
+        self.assertEquals(self.skill.
                           get_formatted_time(float(date.strftime('%s'))),
                           "1 hours and 59 minutes from now")
 
     def test_formatted_time_today_min(self):
         date = datetime.now() + timedelta(minutes=2)
-        self.assertEquals(ScheduledSkill(name='test').
+        self.assertEquals(self.skill.
                           get_formatted_time(float(date.strftime('%s'))),
                           "1 minutes and 59 seconds from now")
 
     def test_formatted_time_days(self):
         date = datetime.now() + timedelta(days=2)
-        self.assertEquals(ScheduledSkill(name='test').
+        self.assertEquals(self.skill.
                           get_formatted_time(float(date.strftime('%s'))),
                           date.strftime("%A, %B %d, %Y at %H:%M"))
