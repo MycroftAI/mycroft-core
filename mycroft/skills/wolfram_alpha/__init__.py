0a89a9016c78452cab89ed231bb12d65e9c3dadb
Ethan Ward <ethanandrewward@gmail.com>
2016-06-27 16:30:45 -0500

    WA skill will now search again if there is one `didyoumean` in the results (#236)
    * Issues 192 - WA skill will now search again with top alternative
    
    * Issues 192 - Add tests for new method
    
    * Issues 192 - Fix pep8
    
    * Issues 192 - Update test syntax
    
    * Issues 192 - Change WA skill to use dialog
    
    * Issues 192 - Address feedback

--- mycroft/skills/wolfram_alpha/__init__.py
+++ mycroft/skills/wolfram_alpha/__init__.py
@@ -28,6 +28,7 @@ from mycroft.identity import IdentityManager
 from mycroft.skills.core import MycroftSkill
 from mycroft.util import CerberusAccessDenied
 from mycroft.util.log import getLogger
+from mycroft.messagebus.message import Message
 
 __author__ = 'seanfitz'
 
@@ -156,12 +157,13 @@ class WolframAlphaSkill(MycroftSkill):
         try:
             res = self.client.query(query)
             result = self.get_result(res)
+            others = self._find_did_you_mean(res)
         except CerberusAccessDenied as e:
             self.speak_dialog('not.paired')
             return
         except Exception as e:
             logger.exception(e)
-            self.speak("Sorry, I don't understand your request.")
+            self.speak_dialog("not.understood")
             return
 
         if result:
@@ -184,7 +186,15 @@ class WolframAlphaSkill(MycroftSkill):
 
             self.speak(response)
         else:
-            self.speak("Sorry, I don't understand your request.")
+            if len(others) > 0:
+                self.speak_dialog('search.again',
+                                  data={'utterance': utterance, 'alternative':
+                                        others[0]})
+                self.handle_fallback(Message('intent_failure',
+                                             metadata={'utterance':
+                                                       others[0]}))
+            else:
+                self.speak_dialog("not.understood")
 
     @staticmethod
     def __find_pod_id(pods, pod_id):
@@ -192,6 +202,22 @@ class WolframAlphaSkill(MycroftSkill):
             if pod_id in pod.id:
                 return pod.text
         return None
+
+    @staticmethod
+    def __find_num(pods, pod_num):
+        for pod in pods:
+            if pod.node.attrib['position'] == pod_num:
+                return pod.text
+        return None
+
+    @staticmethod
+    def _find_did_you_mean(res):
+        value = []
+        root = res.tree.find('didyoumeans')
+        if root is not None:
+            for result in root:
+                value.append(result.text)
+        return value
 
     @staticmethod
     def process_wolfram_string(text):
@@ -207,13 +233,6 @@ class WolframAlphaSkill(MycroftSkill):
         # Convert !s to factorial
         text = re.sub(r"!", r",factorial", text)
         return text
-
-    @staticmethod
-    def __find_num(pods, pod_num):
-        for pod in pods:
-            if pod.node.attrib['position'] == pod_num:
-                return pod.text
-        return None
 
     def stop(self):
         pass
