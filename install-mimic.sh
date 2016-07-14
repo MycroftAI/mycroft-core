ef453ef61d3ea3478587cb7017817452aed42bc1
Arron Atchison <arron.atchison@gmail.com>
2016-07-14 13:39:17 -0500

    temporarily removing  from install-mimic.sh to avoid overwhelming build machines and failing out

--- install-mimic.sh
+++ install-mimic.sh
@@ -10,12 +10,12 @@ if [ ! -d ${MIMIC_DIR} ]; then
     git clone https://github.com/MycroftAI/mimic.git
     cd ${MIMIC_DIR}
     ./configure --with-audio=alsa
-    make -j$CORES
+    make #-j$CORES
 else
     # ensure mimic is up to date
     cd ${MIMIC_DIR}
     git pull
     make clean
     ./configure --with-audio=alsa
-    make -j$CORES
+    make #-j$CORES
 fi
