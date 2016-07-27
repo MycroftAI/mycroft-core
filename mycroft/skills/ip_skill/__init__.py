eb1c8066506693fd2d4f57ceeb11e4bf761c27e1
Isaac Ward <isaacnward@gmail.com>
2016-07-27 17:29:20 -0500

    altered timing

--- mycroft/skills/ip_skill/__init__.py
+++ mycroft/skills/ip_skill/__init__.py
@@ -55,13 +55,12 @@ class IPSkill(MycroftSkill):
                 updated_addresses = [re.sub(r"\.", r" dot ", address)
                              for address in addresses]
                 logger.debug(addresses[0])
-                for address in addresses:
-                    self.enclosure.mouth_text(address)
-                time.sleep(10)
                 self.speak('%s: %s' % (
                     "interface: " + ifaceName +
                     ", I.P. Address ", ', '.join(updated_addresses)))
-
+                for address in addresses:
+                    self.enclosure.mouth_text(address)
+                    time.sleep(10)
                 self.enclosure.activate_mouth_listeners(True)
         self.speak("Those are all my I.P. addresses.")
 
