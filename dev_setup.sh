306d15054334089c1768db9119dfa785e16307eb
Arron Atchison <arron.atchison@gmail.com>
2016-07-29 12:21:03 -0500

    addded a script for installing pocketshinx and changeing the import path in local_recognizer.py

--- dev_setup.sh
+++ dev_setup.sh
@@ -50,24 +50,9 @@ pip install -r requirements.txt
 CORES=$(nproc)
 echo Building with $CORES cores.
 
-# clone pocketsphinx-python at HEAD (fix to a constant version later)
-#if [ ! -d ${TOP}/pocketsphinx-python ]; then
-  # build sphinxbase and pocketsphinx if we haven't already
-#  git clone --recursive https://github.com/cmusphinx/pocketsphinx-python
-#  cd ${TOP}/pocketsphinx-python/sphinxbase
-#  ./autogen.sh
-#  ./configure
-#  make -j$CORES
-#  cd ${TOP}/pocketsphinx-python/pocketsphinx
-#  ./autogen.sh
-#  ./configure
-#  make -j$CORES
-#fi
-
-# build and install pocketsphinx python bindings
-#cd ${TOP}/pocketsphinx-python
-#python setup.py install
-
+#build and install pocketsphinx
+cd ${TOP}
+${TOP}/scripts/install-pocketsphinx.sh -q
 #build and install mimic
 cd ${TOP}
 ${TOP}/scripts/install-mimic.sh
