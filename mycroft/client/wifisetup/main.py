d92395df29842ff26f5a71ccd66e228f880a602b
Arron Atchison <arron.atchison@gmail.com>
2016-08-12 12:12:09 -0500

    client is running, now to handle the rest

--- mycroft/client/wifisetup/main.py
+++ mycroft/client/wifisetup/main.py
@@ -1,18 +1,63 @@
-#!/usr/bin/env python
+# Copyright 2016 Mycroft AI, Inc.
+#
+# This file is part of Mycroft Core.
+#
+# Mycroft Core is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Mycroft Core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
+import subprocess
+import sys
+from Queue import Queue
+from alsaaudio import Mixer
+from threading import Thread
 
+import os
+import serial
+import time
+
+import threading
+
+from mycroft.client.enclosure.arduino import EnclosureArduino
+from mycroft.client.enclosure.eyes import EnclosureEyes
+from mycroft.client.enclosure.mouth import EnclosureMouth
+from mycroft.client.enclosure.weather import EnclosureWeather
+from mycroft.configuration import ConfigurationManager
+from mycroft.messagebus.client.ws import WebsocketClient
+from mycroft.messagebus.message import Message
+from mycroft.util import kill, str2bool
+from mycroft.util import play_wav
+from mycroft.util.log import getLogger
+from mycroft.util.audio_test import record
+
+__author__ = 'aatchison'
+
+#mycroft stuff
+from mycroft.configuration import ConfigurationManager
+from mycroft.messagebus.client.ws import WebsocketClient
+from mycroft.messagebus.message import Message
+from mycroft.util import kill, str2bool
+from mycroft.util.log import getLogger
+
+#wifi setup stuff
 import Queue
 import os
-import signal
 import sys
 import threading
 import time
 from operator import itemgetter
-
 import tornado.ioloop
 import tornado.template
 import tornado.web
 import tornado.websocket
-
 from app.util.Config import AppConfig
 from app.util.FileUtils import ap_mode_config, write_hostapd_conf, write_network_interfaces, write_dnsmasq
 from app.util.LinkUtils import ScanForAP, link_add_vap
@@ -21,11 +66,14 @@ from app.util.dnsmasqTools import dnsmasqTools
 from app.util.hostAPDTools import hostAPServerTools
 from app.util.Server import MainHandler, JSHandler, BootstrapMinJSHandler, BootstrapMinCSSHandler, WSHandler
 from app.util.wpaCLITools import wpaClientTools
+#
 
-config = AppConfig()
-config.open_file()
-Port = config.ConfigSectionMap("server_port")['port']
-WSPort = config.ConfigSectionMap("server_port")['ws_port']
+LOGGER = getLogger("WiFiSetupClient")
+
+#config = AppConfig()
+#config.open_file()
+#Port = config.ConfigSectionMap("server_port")['port']
+#WSPort = config.ConfigSectionMap("server_port")['ws_port']
 dev_link_tools = dev_link_tools()
 linktools = ap_link_tools()
 
@@ -44,6 +92,61 @@ settings = dict(
 )
 
 exitFlag = 0
+
+class WiFiSetup:
+    def __init__(self):
+        self.client = WebsocketClient()
+        self.__register_wifi_events()
+    def setup(self):
+        must_start_ap_mode = True
+        if must_start_ap_mode is not None and must_start_ap_mode is True:
+            LOGGER.info("Initalizing wireless setup mode.")
+            self.client.emit(Message("speak", metadata={
+                'utterance': "Initializing wireless setup mode."}))
+    def run(self):
+        try:
+            self.client.run_forever()
+        except Exception as e:
+            LOGGER.error("Client error: {0}".format(e))
+            self.stop()
+
+    @staticmethod
+    def stop(self):
+        LOGGER.info("Shut down wireless setup mode.")
+
+    def __register_events(self):
+        self.client.on('recognizer_loop:record_begin', self.__update_events)
+        self.__register_wifi_events()
+
+
+    def __wifi_listeners(self, event=None):
+        if event and event.metadata:
+            active = event.metadata['active']
+            if active:
+                self.__register_wifi_events()
+            else:
+                self.__remove_wifi_events()
+
+
+    def __register_wifi_events(self):
+        self.client.on('recognizer_loop:record_begin',self.test_event())
+
+
+    def __remove_wifi_events(self):
+        self.client.remove('recognizer_loop:record_begin', self.test_event())
+
+    def __update_events(self, event=None):
+        if event and event.metadata:
+            if event.metadata.get('paired', False):
+                self.__register_wifi_events()
+            else:
+                self.__remove_wifi_events()
+
+    def test_event(self):
+        print "event triggered"
+
+
+
 
 class tornadoWorker (threading.Thread):
     def __init__(self, threadID, name, q):
@@ -112,6 +215,7 @@ class dnsmasqWorker (threading.Thread):
             S.dnsmasq_on()
         except:
             exit(0)
+
 class Station():
     def __init__(self):
         self.aptools = hostapd_tools()
@@ -192,49 +296,65 @@ workQueue = Queue.Queue(10)
 threads = []
 threadID = 1
 
+def main():
+    try:
+        wifi_setup = WiFiSetup()
+        t = threading.Thread(target=wifi_setup.run)
+        t.start()
+        wifi_setup.setup()
+        t.join()
+    except Exception as e:
+        print (e)
+    finally:
+        sys.exit()
+
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, exit_gracefully)
+    main()
+
+
+
+##    signal.signal(signal.SIGINT, exit_gracefully)
 
 
     # New
-    WPATools = wpaClientTools()
-    APTools = hostAPServerTools()
-    DNSTools = dnsmasqTools()
+##    WPATools = wpaClientTools()
+##    APTools = hostAPServerTools()
+##    DNSTools = dnsmasqTools()
 
-    S = Station()
-    S.station_mode_on()
+##    S = Station()
+##    S.station_mode_on()
 
     # new vars
 
-    INIT = True
+##    INIT = True
     #try_connect()
-    if INIT is True:
-        ap = ScanForAP("AP SCAN: ", 'uap0')
+##    if INIT is True:
+##        ap = ScanForAP("AP SCAN: ", 'uap0')
 
-        thread = apWorker(threadID, 'ap', workQueue)
-        thread.setDaemon(True)
-        thread.start()
-        threads.append(thread)
-        threadID += 1
-        thread.join()
+##        thread = apWorker(threadID, 'ap', workQueue)
+##        thread.setDaemon(True)
+##        thread.start()
+##        threads.append(thread)
+##        threadID += 1
+##        thread.join()
 
-        thread = tornadoWorker(threadID, 'web', workQueue)
-        thread.setDaemon(True)
-        thread.start()
-        threads.append(thread)
-        threadID += 1
+##        thread = tornadoWorker(threadID, 'web', workQueue)
+##        thread.setDaemon(True)
+##        thread.start()
+##        threads.append(thread)
+##        threadID += 1
         #thread.join()
 
 
-    while INIT is True:
-        print "ok"
-        try:
-            if WiFi.wpa_cli_status('wlan0')['wpa_state'] == 'COMPLETED':
-                print "CONNECTED"
-                INIT = False
-        except:
-            print "no"
-            time.sleep(1)
+##    while INIT is True:
+##        print "ok"
+##        try:
+##            if WiFi.wpa_cli_status('wlan0')['wpa_state'] == 'COMPLETED':
+##                print "CONNECTED"
+##                INIT = False
+##        except:
+##            print "no"
+##            time.sleep(1)
 
 
         #or t in threads:
--- start.sh
+++ start.sh
@@ -13,9 +13,9 @@ case $1 in
 	"unittest") SCRIPT=${TOP}/test/test_runner.py ;;
 	"audioaccuracytest") SCRIPT=${TOP}/audio-accuracy-test/audio_accuracy_test.py ;;
 	"sdkdoc") SCRIPT=${TOP}/doc/generate_sdk_docs.py ;;
-        "enclosure") SCRIPT=${TOP}/mycroft/client/enclosure/enclosure.py ;;
-        "pairing") SCRIPT=${TOP}/mycroft/pairing/client.py ;;
-        "wifisetup") SCRIPT=${TOP}/mycroft/client/wifisetup/wifisetup.py ;;
+    "enclosure") SCRIPT=${TOP}/mycroft/client/enclosure/enclosure.py ;;
+    "pairing") SCRIPT=${TOP}/mycroft/pairing/client.py ;;
+    "wifisetup") SCRIPT=${TOP}/mycroft/client/wifisetup/main.py ;;
 	*) echo "Usage: start.sh [service | skills | skill_container | voice | cli | audiotest | collector | unittest | enclosure | pairing | sdkdoc wifisetup]"; exit ;;
 esac
 
