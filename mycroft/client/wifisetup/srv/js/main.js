54a06d7d65e2adef059f37f28a23d78e7c46f291
Augusto Monteiro <augustomna2010@gmail.com>
2016-09-04 16:20:57 -0300

    Issues 356 - connecting

--- mycroft/client/wifisetup/srv/js/main.js
+++ mycroft/client/wifisetup/srv/js/main.js
@@ -18,15 +18,40 @@ function getImagePath(strength) {
     }
 }
 
+function showPanel(id) {
+    var panels = document.querySelectorAll(".panel");
+    Object.keys(panels).forEach(function (panel) {
+        panels[panel].classList.add("hide");
+    });
+    document.querySelector("#" + id).classList.remove("hide");
+}
+
+function showError(message) {
+    var formError = document.querySelector("#formError");
+    formError.classList.add("show");
+    formError.innerHTML = message;
+    setTimeout(function () {
+        formError.classList.remove("show");
+        formError.innerHTML = "";
+    }, 2000);
+}
+
 var WifiSetup = {
+
+    selectedNetword: null,
 
     setListeners: function () {
         WS.addMessageListener("mycroft.wifi.connected", this.onConnected.bind(this));
         WS.addMessageListener("mycroft.wifi.scanned", this.onScanned.bind(this));
     },
 
-    onConnected: function () {
-        console.log("CONNECTED");
+    onConnected: function (data) {
+        if (data.connected) {
+            alert("Connected :D");
+        } else {
+            showPanel("connect");
+            showError("Cannot connect on wifi network");
+        }
     },
 
     onScanned: function (data) {
@@ -37,6 +62,8 @@ var WifiSetup = {
             span = null,
             imgSignal = null,
             imgLock = null;
+
+        showPanel("list-panel");
 
         Object.keys(networks).forEach(function (network) {
             li = document.createElement("li");
@@ -53,6 +80,7 @@ var WifiSetup = {
                 imgLock.className = "lock";
                 li.appendChild(imgLock);
             }
+            networks[network].ssid = network;
             li.addEventListener("click", this.clickNetwork.bind(this, networks[network]));
             fragment.appendChild(li);
         }.bind(this));
@@ -62,7 +90,8 @@ var WifiSetup = {
     },
 
     clickNetwork: function (network) {
-        console.log(network);
+        this.selectedNetword = network;
+        showPanel("connect");
     },
 
     sendScan: function () {
@@ -76,66 +105,80 @@ var WifiSetup = {
         WS.send("mycroft.wifi.connect", data);
     },
 
-    showList: function () {
-
+    clickConnect: function () {
+        showPanel("loading");
+        var pass = document.querySelector("#pass");
+        this.sendConnect({
+            ssid: this.selectedNetword.ssid,
+            pass: pass.value
+        });
     },
 
     init: function () {
         this.setListeners();
-        // this.sendScan();
+        this.sendScan();
+        document.querySelector("#backList").addEventListener("click", function () {
+            this.selectedNetword = {};
+            this.sendScan();
+        }.bind(this));
+        document.querySelector("#passBtn").addEventListener("click", this.clickConnect.bind(this));
     }
 };
 
 window.addEventListener("load", function () {
     WS.connect();
-    // WS.setOnOpenListener(function () {
+    WS.setOnOpenListener(function () {
         WifiSetup.init();
-        WS.onMessage({
-            "message_type": "mycroft.wifi.scanned",
-            "metadata": {
-                "networks": {
-                    "augusto": {
-                        "quality": 0.1,
-                        "encrypted": true
-                    },
-                    "vizinho": {
-                        "quality": 0.2,
-                        "encrypted": false
-                    },
-                    "mycroft": {
-                        "quality": 0.3,
-                        "encrypted": true
-                    },
-                    "android ap": {
-                        "quality": 0.4,
-                        "encrypted": false
-                    },
-                    "iphone ap": {
-                        "quality": 0.5,
-                        "encrypted": true
-                    },
-                    "cavalo de troia": {
-                        "quality": 0.6,
-                        "encrypted": false
-                    },
-                    "google camp": {
-                        "quality": 0.7,
-                        "encrypted": true
-                    },
-                    "j camp": {
-                        "quality": 0.8,
-                        "encrypted": false
-                    },
-                    "charon camp": {
-                        "quality": 0.9,
-                        "encrypted": true
-                    },
-                    "charon camp 2": {
-                        "quality": 1,
-                        "encrypted": false
-                    }
+    });
+});
+
+
+function fakeSend() {
+    WS.onMessage({
+        "message_type": "mycroft.wifi.scanned",
+        "metadata": {
+            "networks": {
+                "augusto": {
+                    "quality": 0.1,
+                    "encrypted": true
+                },
+                "vizinho": {
+                    "quality": 0.2,
+                    "encrypted": false
+                },
+                "mycroft": {
+                    "quality": 0.3,
+                    "encrypted": true
+                },
+                "android ap": {
+                    "quality": 0.4,
+                    "encrypted": false
+                },
+                "iphone ap": {
+                    "quality": 0.5,
+                    "encrypted": true
+                },
+                "cavalo de troia": {
+                    "quality": 0.6,
+                    "encrypted": false
+                },
+                "google camp": {
+                    "quality": 0.7,
+                    "encrypted": true
+                },
+                "j camp": {
+                    "quality": 0.8,
+                    "encrypted": false
+                },
+                "charon camp": {
+                    "quality": 0.9,
+                    "encrypted": true
+                },
+                "charon camp 2": {
+                    "quality": 1,
+                    "encrypted": false
                 }
             }
-        });
-    // });
-});
+        }
+    });
+}
