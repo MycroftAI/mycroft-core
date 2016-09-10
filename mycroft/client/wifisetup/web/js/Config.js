9fc20510d7b94961f6f18532c869c4499e5416b5
Augusto Monteiro <augustomna2010@gmail.com>
2016-09-10 00:11:17 -0300

    Issues 356 - create succes page and merge connect page on list page

--- mycroft/client/wifisetup/web/js/Config.js
+++ mycroft/client/wifisetup/web/js/Config.js
@@ -1,3 +1,4 @@
 var Config = {
-    wsUrl: "ws://172.24.1.1:8000/events/ws"
+    //wsUrl: "ws://172.24.1.1:8000/events/ws"
+    wsUrl: "ws://192.168.1.3:8080"
 };
--- mycroft/client/wifisetup/web/js/main.js
+++ mycroft/client/wifisetup/web/js/main.js
@@ -1,9 +1,3 @@
-/*
- *  SEND {message_type: "mycroft.wifi.scan"}
- *  SEND {message_type: "mycroft.wifi.connect", metadata: {ssid: "", pass: ""}}
- *  LISTEN {message_type: "mycroft.wifi.connected", metadata: {connected: boolean}}
- *  LISTEN {message_type: "mycroft.wifi.scanned", metadata: {networks: Map<SSID, {quality: "Int/Int", encrypted: "boolean"}>>}}
- * */
 function getImagePath(strength) {
     if (strength > 0.8) {
         return "img/wifi_4.png";
@@ -26,118 +20,185 @@ function showPanel(id) {
     document.querySelector("#" + id).classList.remove("hide");
 }
 
-function showError(message) {
-    var formError = document.querySelector("#formError");
-    formError.classList.add("show");
-    formError.innerHTML = message;
-    setTimeout(function () {
-        formError.classList.remove("show");
-        formError.innerHTML = "";
-    }, 2000);
-}
-
 var WifiSetup = {
 
-    selectedNetword: null,
+        selectedNetword: null,
 
-    setListeners: function () {
-        WS.addMessageListener("mycroft.wifi.connected", this.onConnected.bind(this));
-        WS.addMessageListener("mycroft.wifi.scanned", this.onScanned.bind(this));
-    },
+        setListeners: function () {
+            WS.addMessageListener("mycroft.wifi.connected", this.onConnected.bind(this));
+            WS.addMessageListener("mycroft.wifi.scanned", this.onScanned.bind(this));
+        },
 
-    onConnected: function (data) {
-        if (data.connected) {
-            alert("Connected :D");
-        } else {
-            if (document.querySelector("#list-panel").classList.contains("hide")) {
-                showError("Cannot connect on wifi network");
+        onConnected: function (data) {
+            if (data.connected) {
+                showPanel("success");
             } else {
-                alert("Cannot connect on wifi network");
+                showPanel("list-panel");
+                this.renderErrorItem(this.selectedNetword.el);
             }
-        }
-    },
+        },
 
-    onScanned: function (data) {
-        var networks = data.networks,
-            fragment = document.createDocumentFragment(),
-            list = document.querySelector("#list"),
-            li = null,
-            span = null,
-            imgSignal = null,
-            imgLock = null;
+        onScanned: function (data) {
+            var networks = data.networks,
+                fragment = document.createDocumentFragment(),
+                list = document.querySelector("#list"),
+                item = null,
+                li = null;
 
-        showPanel("list-panel");
+            showPanel("list-panel");
 
-        Object.keys(networks).sort(function(a,b){
-            if (networks[a].quality < networks[b].quality){
-                return 1;
-            }
-            return 0;
-        }).forEach(function (network) {
-            li = document.createElement("li");
-            span = document.createElement("span");
-            imgSignal = document.createElement("img");
-            span.textContent = network;
-            imgSignal.src = getImagePath(networks[network].quality);
+            Object.keys(networks).sort(function (a, b) {
+                if (networks[a].quality < networks[b].quality) {
+                    return 1;
+                }
+                return 0;
+            }).forEach(function (network) {
+                li = document.createElement("li");
+                networks[network].ssid = network;
+                networks[network].el = li;
+                item = this.renderListItem(networks[network]);
+                li.appendChild(item);
+                fragment.appendChild(li);
+            }.bind(this));
+
+            list.innerHTML = null;
+            list.appendChild(fragment);
+        },
+
+        renderListItem: function (network) {
+            var listItem = document.createElement("div"),
+                span = document.createElement("span"),
+                imgSignal = document.createElement("img");
+            listItem.className = "list-item show";
+            span.textContent = network.ssid;
+            imgSignal.src = getImagePath(network.quality);
             imgSignal.className = "wifi";
-            li.appendChild(span);
-            li.appendChild(imgSignal);
-            if (networks[network].encrypted) {
-                imgLock = document.createElement("img");
+            listItem.appendChild(span);
+            listItem.appendChild(imgSignal);
+            if (network.connected) {
+                var connected = document.createElement("span");
+                connected.className = "connected";
+                connected.textContent = "Connected";
+                listItem.classList.add("connected");
+                listItem.appendChild(connected);
+            } else {
+                listItem.addEventListener("click", this.clickNetwork.bind(this, network));
+            }
+            if (network.encrypted) {
+                var imgLock = document.createElement("img");
                 imgLock.src = "img/lock.png";
                 imgLock.className = "lock";
-                li.appendChild(imgLock);
+                listItem.appendChild(imgLock);
             }
-            networks[network].ssid = network;
-            li.addEventListener("click", this.clickNetwork.bind(this, networks[network]));
-            fragment.appendChild(li);
-        }.bind(this));
+            return listItem;
+        },
 
-        list.innerHTML = null;
-        list.appendChild(fragment);
-    },
+        ItemDefaultState: function () {
+            var li = document.querySelector(".list-item:not(.show)");
+            if (!li) {
+                return;
+            }
+            var divs = li.parentNode.childNodes;
 
-    clickNetwork: function (network) {
-        this.selectedNetword = network;
-        if (network.encrypted) {
-            showPanel("connect");
-        } else {
-            this.sendConnect({
-                ssid: network.ssid
+            Object.keys(divs).forEach(function (div) {
+                divs[div].classList.remove("show");
+                if (divs[div].classList.contains("list-item")) {
+                    divs[div].classList.add("show");
+                }
             });
+        },
+
+        renderConnectItem: function (li) {
+            var connect = li.querySelector(".connect-item");
+            if (connect) {
+                li.querySelector(".list-item").classList.remove("show");
+                connect.classList.add("show");
+                return;
+            }
+            connect = document.createElement("div");
+            var imgArrow = document.createElement("img");
+            var label = document.createElement("label");
+            connect.className = "connect-item";
+            imgArrow.src = "img/next.png";
+            imgArrow.addEventListener("click", this.clickConnect.bind(this));
+            connect.appendChild(label);
+            if (this.selectedNetword.encrypted) {
+                passwordInput = document.createElement("input");
+                label.textContent = "Password: ";
+                passwordInput.type = "password";
+                connect.appendChild(passwordInput);
+            } else {
+                label.className = "public";
+                label.textContent = this.selectedNetword.ssid;
+            }
+            connect.appendChild(imgArrow);
+            li.appendChild(connect);
+            li.querySelector(".list-item").classList.remove("show");
+            connect.classList.add("show");
+        },
+
+        renderErrorItem: function (li) {
+            var error = li.querySelector(".error-item");
+            if (error) {
+                li.querySelector(".connect-item").classList.remove("show");
+                error.classList.add("show");
+                return;
+            }
+            error = document.createElement("div");
+            var imgClose = document.createElement("img");
+            error.classList.add("error-item");
+            imgClose.src = "img/error.png";
+            var message = document.createElement("span");
+            message.textContent = "Try again or connect to a diferent wifi.";
+            error.appendChild(message);
+            error.appendChild(imgClose);
+            li.appendChild(error);
+            li.querySelector(".connect-item").classList.remove("show");
+            error.classList.add("show");
+            error.addEventListener("click", this.ItemDefaultState);
+        },
+
+        clickNetwork: function (network) {
+            this.selectedNetword = network;
+            this.ItemDefaultState();
+            this.renderConnectItem(network.el);
         }
-    },
+        ,
 
-    sendScan: function () {
-        showPanel("loading");
-        WS.send("mycroft.wifi.scan");
-    },
+        sendScan: function () {
+            showPanel("loading");
+            WS.send("mycroft.wifi.scan");
+        }
+        ,
 
-    /***
-     * @param data is a object with ssid and pass
-     */
-    sendConnect: function (data) {
-        WS.send("mycroft.wifi.connect", data);
-    },
+        /***
+         * @param data is a object with ssid and pass
+         */
+        sendConnect: function (data) {
+            WS.send("mycroft.wifi.connect", data);
+        }
+        ,
 
-    clickConnect: function () {
-        var pass = document.querySelector("#pass");
-        this.sendConnect({
-            ssid: this.selectedNetword.ssid,
-            pass: pass.value
-        });
-    },
+        clickConnect: function () {
+            showPanel("connecting");
+            var network = {
+                ssid: this.selectedNetword.ssid
+            };
+            if (this.selectedNetword.encrypted) {
+                var pass = this.selectedNetword.el.querySelector("input");
+                network.pass = pass.value;
+            }
+            this.sendConnect(network);
+        }
+        ,
 
-    init: function () {
-        this.setListeners();
-        this.sendScan();
-        document.querySelector("#backList").addEventListener("click", function () {
-            this.selectedNetword = {};
-            this.sendScan();
-        }.bind(this));
-        document.querySelector("#passBtn").addEventListener("click", this.clickConnect.bind(this));
+        init: function () {
+            this.setListeners();
+            showPanel("home");
+            document.querySelector("#connectBtn").addEventListener("click", this.sendScan);
+        }
     }
-};
+    ;
 
 window.addEventListener("load", function () {
     WS.connect();
