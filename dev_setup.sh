67b920b7c0d626daac25ec2d84aab21efc917a8c
ParkerMc <ParkerMc@users.noreply.github.com>
2016-09-15 18:34:28 -0500

    Changed pip to pip2
    needed when python 3 is default

--- dev_setup.sh
+++ dev_setup.sh
@@ -45,7 +45,7 @@ cd ${TOP}
 easy_install pip==7.1.2 # force version of pip
 
 # install requirements (except pocketsphinx)
-pip install -r requirements.txt 
+pip2 install -r requirements.txt 
 
 CORES=$(nproc)
 echo Building with $CORES cores.
