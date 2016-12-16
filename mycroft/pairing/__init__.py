0701830ada79f81edfe39ed1322b8210f8ddc058
Arron Atchison <arron.atchison@gmail.com>
2016-12-16 12:05:58 -0600

    Changed License.md to GNU Lesser Public liscense; Replaced GPL v3 in file headers

--- mycroft/pairing/__init__.py
+++ mycroft/pairing/__init__.py
@@ -3,14 +3,14 @@
 # This file is part of Mycroft Core.
 #
 # Mycroft Core is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# it under the terms of the GNU Lesser Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # Mycroft Core is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Lesser Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
+# You should have received a copy of the GNU Lesser Public License
 # along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
