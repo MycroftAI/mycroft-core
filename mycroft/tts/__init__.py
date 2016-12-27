51f25229fc10e37fb6dbec4d69dac5ce5fc72d13
Åke Forslund <ake.forslund@gmail.com>
2016-12-27 10:44:35 +0100

    Add pymimic tts backend
    Currently this is very basic and doesn't support visimes or things like duration stretch. pymimic supports these but currently the internal blocking playback is used which hinders the use of visimes.

--- mycroft/tts/__init__.py
+++ mycroft/tts/__init__.py
@@ -321,6 +321,7 @@ class TTSFactory(object):
     from mycroft.tts.mary_tts import MaryTTS
     from mycroft.tts.mimic_tts import Mimic
     from mycroft.tts.spdsay_tts import SpdSay
+    from mycroft.tts.pymimic_tts import Pymimic
 
     CLASSES = {
         "mimic": Mimic,
@@ -328,7 +329,8 @@ class TTSFactory(object):
         "marytts": MaryTTS,
         "fatts": FATTS,
         "espeak": ESpeak,
-        "spdsay": SpdSay
+        "spdsay": SpdSay,
+        "pymimic": Pymimic
     }
 
     @staticmethod
