91d2770e7727be7015fc50584ba9d72e80146445
Åke Forslund <ake.forslund@gmail.com>
2016-12-28 14:00:20 +0100

    install-mimic picks a specific version, not the latest

--- scripts/install-mimic.sh
+++ scripts/install-mimic.sh
@@ -4,10 +4,11 @@ set -Ee
 
 MIMIC_DIR=mimic
 CORES=$(nproc)
+MIMIC_VERSION=1.1.0
 
 # download and install mimic
 if [ ! -d ${MIMIC_DIR} ]; then
-    git clone https://github.com/MycroftAI/mimic.git
+    git clone --branch ${MIMIC_VERSION} https://github.com/MycroftAI/mimic.git
     cd ${MIMIC_DIR}
     ./configure --with-audio=alsa
     make #-j$CORES
