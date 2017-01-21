dc790763d6fd1e3a3edaf8e34c7364e1d507163b
tmajibon <tmajibon@gmail.com>
2017-01-21 15:01:16 -0600

    Listen confirmation
    If enabled, will play a wave file to confirm that Mycroft is listening

--- mycroft/client/speech/main.py
+++ mycroft/client/speech/main.py
@@ -27,7 +27,7 @@ from mycroft.identity import IdentityManager
 from mycroft.messagebus.client.ws import WebsocketClient
 from mycroft.messagebus.message import Message
 from mycroft.tts import TTSFactory
-from mycroft.util import kill
+from mycroft.util import kill, play_wav
 from mycroft.util.log import getLogger
 
 logger = getLogger("SpeechClient")
@@ -41,6 +41,12 @@ config = ConfigurationManager.get()
 
 def handle_record_begin():
     logger.info("Begin Recording...")
+
+    # If enabled, play a wave file with a short sound to indicate recording has
+    # begun.
+    if config.get('confirm_keyword'):
+        play_wav(expanduser(config.get('confirm_ding')))
+        
     ws.emit(Message('recognizer_loop:record_begin'))
 
 
