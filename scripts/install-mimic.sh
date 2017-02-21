91137e6635f883d8a6ac94996a3d7fe55953b029
Arron Atchison <arron.atchison@gmail.com>
2017-02-21 10:09:09 -0600

    Updating Mimic version to 1.2.0; using --enable-shared flag for future Pymimic support

--- scripts/install-mimic.sh
+++ scripts/install-mimic.sh
@@ -4,19 +4,19 @@ set -Ee
 
 MIMIC_DIR=mimic
 CORES=$(nproc)
-MIMIC_VERSION=1.1.0
+MIMIC_VERSION=1.2.0
 
 # download and install mimic
 if [ ! -d ${MIMIC_DIR} ]; then
     git clone --branch ${MIMIC_VERSION} https://github.com/MycroftAI/mimic.git
     cd ${MIMIC_DIR}
-    ./configure --with-audio=alsa
+    ./configure --with-audio=alsa --enable-shared
     make #-j$CORES
 else
     # ensure mimic is up to date
     cd ${MIMIC_DIR}
     git pull
     make clean
-    ./configure --with-audio=alsa
+    ./configure --with-audio=alsa --enable-shared
     make #-j$CORES
 fi
