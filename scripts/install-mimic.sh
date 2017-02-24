927118061f9072a66e86ee069bafebd43ba9362c
Arron Atchison <arron.atchison@gmail.com>
2017-02-24 12:34:15 -0600

    Update install-mimic.sh

--- scripts/install-mimic.sh
+++ scripts/install-mimic.sh
@@ -6,6 +6,10 @@ MIMIC_DIR=mimic
 CORES=$(nproc)
 MIMIC_VERSION=1.2.0.1
 
+# for ubuntu precise in travis, that does not provide pkg-config:
+pkg-config --exists icu-i18n || export CFLAGS="$CFLAGS -I/usr/include/x86_64-linux-gnu"
+pkg-config --exists icu-i18n || export LDFLAGS="$LDFLAGS -licui18n -licuuc -licudata"
+
 # download and install mimic
 if [ ! -d ${MIMIC_DIR} ]; then
     git clone --branch ${MIMIC_VERSION} https://github.com/MycroftAI/mimic.git
