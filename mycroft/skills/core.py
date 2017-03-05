ccfdab3d4e66e8df42bc3322050a46090dd7d26f
JarbasAI <somom@30wave.com>
2017-03-05 17:10:41 -0600

    pep8

--- mycroft/skills/core.py
+++ mycroft/skills/core.py
@@ -302,7 +302,8 @@ class MycroftSkill(object):
     def stop(self):
         pass
 
-    def Converse(self, transcript, lang="en-us"):  # TODO read language from config?
+    def Converse(self, transcript, lang="en-us"):
+        # TODO read language from config?
         return False
 
     def is_stop(self):
