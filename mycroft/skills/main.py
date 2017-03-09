d1a7167e2a54384bc8c945d81808c6eca4cc5bb8
JarbasAI <somom@30wave.com>
2017-03-09 11:57:47 -0600

    Update main.py

--- mycroft/skills/main.py
+++ mycroft/skills/main.py
@@ -108,6 +108,8 @@ def watch_skills():
                         continue
                     elif skill.get(
                             "instance") and modified > last_modified_skill:
+                        if not skill["instance"].reload_skill:
+                            continue
                         logger.debug("Reloading Skill: " + skill_folder)
                         skill["instance"].shutdown()
                         clear_skill_events(skill["instance"])
@@ -130,7 +132,7 @@ def handle_conversation_request(message):
     for skill in loaded_skills:
         if loaded_skills[skill]["id"] == skill_id:
             instance = loaded_skills[skill]["instance"]
-            result = instance.Converse(utterances)
+            result = instance.converse(utterances)
             ws.emit(Message("converse_status_response", {
                     "skill_id": skill_id, "result": result}))
             return
