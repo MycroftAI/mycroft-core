9fce7d4620819b0ab55b839e43a5c8dec18db2ad
penrods <github@gpll.org>
2017-03-10 01:30:15 -0600

    This implements CLI enhancements per issue #547
    Main CLI enhancements:
    * Microphone meter
    * Long log line left/right scrolling
    * Eliminated flicker
    * VT100 ESC key code support (used by some terms)
    
    In addition, to achieve the meter it was necessary to implement a mechanism for local Inter Process Communication (IPC).  This is achieved using the file-system.  By default a folder structure is created under /tmp/mycroft/ipc, but it can be directed to somewhere else by setting the config value in mycroft.conf:
        "ipc_path" : "/path/to/somewhere"
    In the future, Mark 1 and Picroft will get RAM disks to avoid burning out the SD card.  This is also a very fast communication mechanism.  This is all hidden under util.get_ipc_directory()
    
    Further, the named signal mechanism was changed to use the IPC folder.  The signal can have a lifetime now (not just one shot).

--- mycroft/client/speech/mic.py
+++ mycroft/client/speech/mic.py
@@ -30,7 +30,7 @@ from speech_recognition import (
 
 from mycroft.configuration import ConfigurationManager
 from mycroft.util import check_for_signal
-from mycroft.util.log import getLogger
+from mycroft.util.log import getLogger, get_ipc_directory
 
 listener_config = ConfigurationManager.get().get('listener')
 logger = getLogger(__name__)
@@ -148,6 +148,7 @@ class ResponsiveRecognizer(speech_recognition.Recognizer):
         self.audio = pyaudio.PyAudio()
         self.multiplier = listener_config.get('multiplier')
         self.energy_ratio = listener_config.get('energy_ratio')
+        self.mic_level_file = os.path.join(get_ipc_directory(), "mic_level")
 
     @staticmethod
     def record_sound_chunk(source):
@@ -217,6 +218,12 @@ class ResponsiveRecognizer(speech_recognition.Recognizer):
                 noise = decrease_noise(noise)
                 self.adjust_threshold(energy, sec_per_buffer)
 
+            if num_chunks % 10 == 0:
+                with open(self.mic_level_file, 'w') as f:
+                    f.write("Energy:  cur=" + str(energy) + " thresh=" +
+                            str(self.energy_threshold))
+                f.close()
+
             was_loud_enough = num_loud_chunks > min_loud_chunks
             quiet_enough = noise <= min_noise
             recorded_too_much_silence = num_chunks > max_chunks_of_silence
@@ -247,6 +254,7 @@ class ResponsiveRecognizer(speech_recognition.Recognizer):
         max_size = self.sec_to_bytes(self.SAVED_WW_SEC, source)
 
         said_wake_word = False
+        counter = 0
         while not said_wake_word:
             if check_for_signal('buttonPress'):
                 said_wake_word = True
@@ -258,6 +266,17 @@ class ResponsiveRecognizer(speech_recognition.Recognizer):
             if energy < self.energy_threshold * self.multiplier:
                 self.adjust_threshold(energy, sec_per_buffer)
 
+            if counter > 2:
+                with open(self.mic_level_file, 'w') as f:
+                    f.write("Energy:  cur=" + str(energy) + " thresh=" +
+                            str(self.energy_threshold))
+                f.close()
+                counter = 0
+            else:
+                counter += 1
+
+            # At first, the buffer is empty and must fill up.  After that
+            # just drop the first chunk bytes to keep it the same size.
             needs_to_grow = len(byte_data) < max_size
             if needs_to_grow:
                 byte_data += chunk
