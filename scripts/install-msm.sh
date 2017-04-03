c8c3b3d67ff12330d488ef7d90f1b8dcffc50355
Arron Atchison <arron.atchison@gmail.com>
2017-04-03 12:30:47 -0700

    checking to see if opt/mycroft/skills exists and is writable

--- scripts/install-msm.sh
+++ scripts/install-msm.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
+
+SKILLS_DIR='/opt/mycroft/skills'
 # exit on any error
 set -Ee
 rm -rf msm
 git clone https://github.com/MycroftAI/msm.git msm
 chmod +x msm/msm
-sudo mkdir -p /opt/mycroft/skills
-sudo chown $USER:$USER /opt/mycroft/skills
+
+if [ ! -d ${SKILLS_DIR} ]; then
+    sudo chown $USER:$USER ${SKILLS_DIR}
+fi
+
+if [ ! -w ${SKILLS_DIR} ]; then
+	sudo mkdir -p ${SKILLS_DIR}
+fi
+
