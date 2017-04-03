4d8f7f6e13cf63cbdffc0c3257159aaf4704fab7
Augusto Monteiro 'Sparky <augustomna2010@gmail.com>
2017-04-03 11:45:26 -0700

    #630 - Updating msm install script

--- scripts/install-msm.sh
+++ scripts/install-msm.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # exit on any error
 set -Ee
-
+rm -rf msm
 git clone https://github.com/MycroftAI/msm.git msm
 chmod +x msm/msm
+sudo mkdir -p /opt/mycroft/skills
+sudo chown $USER:$USER /opt/mycroft/skills
