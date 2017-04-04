8cecac605317205f7bbb931d72cf832cfe18fbdd
Jason Hines <jason@hinesnetwork.com>
2017-04-03 21:31:22 -0400

    Added build script for Solus

---
+++ build_host_setup_solus.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+sudo eopkg it \
+    git \
+    python \
+    pip \
+    python-setuptools \
+    virtualenv \
+    python-gobject \
+    virtualenvwrapper \
+    libtool \
+    libffi \
+    openssl \
+    autoconf \
+    bison \
+    swig \
+    glib2 \
+    portaudio \
+    mpg123 \
+    screen \
+    libflac \
+    curl
+
+# upgrade virtualenv to latest from pypi
+sudo pip install --upgrade virtualenv
+sudo pip install s3cmd
