5590694e7b7777fbd1f86c5ff5a3b630fa8c533a
Jason Hines <jason@hinesnetwork.com>
2017-04-05 16:34:56 -0400

    Refined Solus build script

--- build_host_setup_solus.sh
+++ build_host_setup_solus.sh
@@ -1,25 +1,28 @@
 #!/usr/bin/env bash
 
+sudo eopkg it -cy system.devel
 sudo eopkg it \
     git \
-    python \
+#    python \
     pip \
     python-setuptools \
     virtualenv \
-    python-gobject \
+#    python-gobject \
     virtualenvwrapper \
-    libtool \
-    libffi \
-    openssl \
-    autoconf \
-    bison \
-    swig \
-    glib2 \
-    portaudio \
+#    libtool \
+#    libffi \
+#    openssl \
+#    autoconf \
+#    bison \
+#    swig \
+#    glib2 \
+    alsa-lib-devel \
+#    portaudio \
+    portaudio-devel \
     mpg123 \
     screen \
-    libflac \
-    curl
+#    libflac \
+#    curl
 
 # upgrade virtualenv to latest from pypi
 sudo pip install --upgrade virtualenv
