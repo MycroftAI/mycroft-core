1087d0e3b4b2430741be5f22827e26c1ed43c05a
Augusto Monteiro 'Sparky <augustomna2010@gmail.com>
2017-04-06 11:23:45 -0700

    #636 - Adding loglevel

--- mycroft/configuration/__init__.py
+++ mycroft/configuration/__init__.py
@@ -15,11 +15,11 @@
 # You should have received a copy of the GNU General Public License
 # along with Mycroft Core.  If not, see <http://www.gnu.org/licenses/>.
 import json
+
+import inflection
 import re
 from genericpath import exists, isfile
 from os.path import join, dirname, expanduser
-
-import inflection
 
 from mycroft.util.log import getLogger
 
