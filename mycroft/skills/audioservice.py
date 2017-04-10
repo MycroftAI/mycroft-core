c81ad57ebf1fc2224f677d02cd97ffd8008dd047
Åke Forslund <ake.forslund@gmail.com>
2017-04-10 16:20:01 +0200

    Populate AudioService class with more methods
    Pause, resume, next and previous added to the interface.
    The play method was updated to add 'file://' to uri's lacking uri type.

--- mycroft/skills/audioservice.py
+++ mycroft/skills/audioservice.py
@@ -1,6 +1,13 @@
 import time
-
+from os.path import abspath
 from mycroft.messagebus.message import Message
+
+
+def ensure_uri(s):
+    if '://' not in s:
+        return 'file://' + abspath(s)
+    else:
+        return s
 
 
 class AudioService():
@@ -13,16 +20,38 @@ class AudioService():
         self.info = message.data
 
     def play(self, tracks=[], utterance=''):
+        """ Start playback.
+            tracks: track uri or list of track uri's
+            utterance: forward utterance for further processing by the audio
+                       service
+        """
         if isinstance(tracks, basestring):
             tracks = [tracks]
         elif not isinstance(tracks, list):
             raise ValueError
-
+        tracks = [ensure_uri(t) for t in tracks]
         self.emitter.emit(Message('MycroftAudioServicePlay',
                                   data={'tracks': tracks,
                                         'utterance': utterance}))
 
+    def next(self):
+        """ Change to next track. """
+        self.emitter.emit(Message('MycroftAudioServiceNext'))
+
+    def prev(self):
+        """ Change to previous track. """
+        self.emitter.emit(Message('MycroftAudioServicePrev'))
+
+    def pause(self):
+        """ Pause playback. """
+        self.emitter.emit(Message('MycroftAudioServicePause'))
+
+    def resume(self):
+        """ Resume paused playback. """
+        self.emitter.emit(Message('MycroftAudioServiceResume'))
+
     def track_info(self):
+        """ Request information of current playing track. """
         self.info = None
         self.emitter.emit(Message('MycroftAudioServiceTrackInfo'))
         while self.info is None:
