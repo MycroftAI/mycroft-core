96332ac77c4903e7e7e5b14701dbf36dde1a41b3
Steve Penrod <github@gpll.org>
2017-04-14 17:40:29 -0700

    Fixing minor issues from review

--- scripts/prepare-msm.sh
+++ scripts/prepare-msm.sh
@@ -7,12 +7,12 @@ chmod +x msm/msm
 
 if [[ ${IS_TRAVIS} != true ]]; then
     if [ ! -d ${SKILLS_DIR} ]; then
-	echo "Create /opt/mycroft/skills"
+        echo "Create /opt/mycroft/skills"
         sudo mkdir -p ${SKILLS_DIR}
     fi
 
     if [ ! -w ${SKILLS_DIR} ]; then
-	echo "Changin ownsership of /opt/mycroft/skills"
+        echo "Changing ownsership of /opt/mycroft/skills"
         sudo chown $USER:$USER ${SKILLS_DIR}
     fi
 fi
