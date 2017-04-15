e3de2fe610ef24fc5dea3ac457def6c9bdc256b7
Åke Forslund <ake.forslund@gmail.com>
2017-04-15 11:50:36 +0200

    Move ignore SIGCHLD to entry points.
    Ignoring SIGCHLD trips up unittests, this was done as the unittest loaded mycroft.skills.core

--- mycroft/skills/main.py
+++ mycroft/skills/main.py
@@ -33,6 +33,12 @@ from mycroft.skills.core import load_skill, create_skill_descriptor, \
 from mycroft.skills.intent_service import IntentService
 from mycroft.util.log import getLogger
 
+from mycroft.lock import Lock  # Creates PID file for single instance
+import signal
+
+# ignore DIGCHLD to terminate subprocesses correctly
+signal.signal(signal.SIGCHLD, signal.SIG_IGN)
+
 logger = getLogger("Skills")
 
 __author__ = 'seanfitz'
