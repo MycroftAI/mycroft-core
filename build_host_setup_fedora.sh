db771aa360a895e4e3b31f6a01d3320880e489a7
Liviu Vasut <liviu.vasut@gmail.com>
2017-04-15 10:51:59 +0300

    Issues-657 - replaced hardcoded fedora version

--- build_host_setup_fedora.sh
+++ build_host_setup_fedora.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo rpm -Uvh http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-23.noarch.rpm
+sudo rpm -Uvh http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 
 sudo dnf install -y \
     git \
