bdf1841a97a63dd0a019b3f13eb4d1ac41b066b3
Jason Hines <jason@hinesnetwork.com>
2017-04-16 14:11:45 -0400

    removed commented packages. Added second eopkg call becuase unstable won't install without it

--- build_host_setup_solus.sh
+++ build_host_setup_solus.sh
@@ -1,27 +1,15 @@
 #!/usr/bin/env bash
 
-sudo eopkg it -y -c system.devel
+sudo eopkg it -y -c system.devel \
     git \
-#    python \
     pip \
     python-setuptools \
     virtualenv \
-#    python-gobject \
     virtualenvwrapper \
-#    libtool \
-#    libffi \
-#    openssl \
-#    autoconf \
-#    bison \
-#    swig \
-#    glib2 \
     alsa-lib-devel \
-#    portaudio \
     portaudio-devel \
     mpg123 \
     screen \
-#    libflac \
-#    curl
 
 # upgrade virtualenv to latest from pypi
 sudo pip install --upgrade virtualenv
