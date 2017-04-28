41e7109ba535c8bd18f5cd2718a81efaadcc57c6
kfezer@gmail.com <kfezer@gmail.com>
2017-04-28 15:08:42 -0700

    modified:   mycroft/client/enclosure/__init__.py

--- mycroft/client/enclosure/__init__.py
+++ mycroft/client/enclosure/__init__.py
@@ -142,7 +142,7 @@ class EnclosureReader(Thread):
             subprocess.call('systemctl reboot -i', shell=True)
 
         if "unit.enable-ssh" in data:
-            subprocess.call('touch /ssh', shell=True)
+            subprocess.call('sudo touch /ssh', shell=True)
             self.ws.emit(Message("speak", {
                 'utterance': "SSH will be enabled on next boot"}))
 
