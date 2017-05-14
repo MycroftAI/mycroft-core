7f3040ee54ccf69a8c0289f79f38ed7cd4a8c2ab
JarbasAI <somom@30wave.com>
2017-05-14 19:27:59 +0100

    Update core.py

--- mycroft/skills/core.py
+++ mycroft/skills/core.py
@@ -108,7 +108,6 @@ def load_skill(skill_descriptor, emitter, skill_id):
             # v2 skills framework
             skill = skill_module.create_skill()
             skill.bind(emitter)
-            skill._dir = dirname(skill_descriptor['info'][1])
             skill.skill_id = skill_id
             skill.load_data_files(dirname(skill_descriptor['info'][1]))
             skill.initialize()
