9b1f2fe7a53b3783734b9426cd08150d2a09145a
Arron Atchison <arron.atchison@gmail.com>
2017-06-13 18:00:45 -0500

    Revert "Refactor setup_base.py to use new versioning method (#802)"
    This reverts commit ba5b255b0ec2c459c90447b19c574813fcc92fdd.

--- mycroft/util/setup_base.py
+++ mycroft/util/setup_base.py
@@ -36,9 +36,8 @@ def place_manifest(manifest_file):
 def get_version():
     version = None
     try:
-        import mycroft.version
-        version = mycroft.version.VersionManager.get()
-        logger.debug(version)
+        import mycroft.__version__
+        version = mycroft.__version__.version
     except Exception as e:
         try:
             version = "dev-" + subprocess.check_output(
