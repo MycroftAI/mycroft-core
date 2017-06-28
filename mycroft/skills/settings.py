e3b4e7c6e38ef5030360e3200685f7c0185c39ae
Michael Nguyen <ppnguyen91@gmail.com>
2017-06-28 11:32:19 -0500

    [WIP] modify settings.py to post metadata to api, and get settings form api

--- mycroft/skills/settings.py
+++ mycroft/skills/settings.py
@@ -30,7 +30,12 @@
 
 import json
 import sys
-from os.path import isfile
+from os.path import isfile, join
+from mycroft.util.log import getLogger
+from mycroft.api import DeviceApi
+
+logger = getLogger(__name__)
+SKILLS_DIR = "/opt/mycroft/skills"
 
 
 class SkillSettings(dict):
@@ -41,16 +46,42 @@ class SkillSettings(dict):
         Args:
             settings_file (str): Path to storage file
     """
-    def __init__(self, settings_file):
+    def __init__(self, directory, name):
         super(SkillSettings, self).__init__()
-        self._path = settings_file
+        self.api = DeviceApi()
+        self.name = name
+        self._device_identity = self.api.identity.uuid
+        self._settings_path = join(directory, 'settings.json')
+        self._meta_path = join(directory, 'settingsmeta.json')
+        self._uuid_path = join('~/.mycroft/skills/' + self.name,
+                               'identity.json')
+        self._api_path = "/" + self._device_identity + "/skill"
 
         # if file exist, open and read stored values into self
-        if isfile(self._path):
-            with open(self._path) as f:
-                json_data = json.load(f)
-                for key in json_data:
-                    self.__setitem__(key, json_data[key])
+        if isfile(self._settings_path):
+            with open(self._settings _path) as f:
+                try:
+                    json_data = json.load(f)
+                    for key in json_data:
+                        self.__setitem__(key, json_data[key])
+                except Exception as e:
+                    # Show error on webUI.  Dev will have to fix
+                    # metadata to be able to edit later.
+                    logger.error(e)
+
+        # if settingsmeta.json exist and loaded for the first time
+        if isfile(self._meta_path) and not isfile(self._uuid_path):
+            with open(self._meta_path) as f:
+                self.settings_meta = json.load(f)
+
+            # use this for testing purposes
+            self.__setitem__('test', 'testing')
+            self.store()
+
+            # # post metadata to tartarus and store uuid for skill
+            # response = self._post_metadata(self.settings_meta)
+            # self.__setitem__('uuid', response['uuid'])
+            # self.store()
 
         self._is_stored = True
 
@@ -69,5 +100,29 @@ class SkillSettings(dict):
             Store dictionary to file if it has changed
         """
         if not self._is_stored:
-            with open(self._path, 'w')as f:
+            with open(self._settings_path, 'w') as f:
                 json.dump(self, f)
+
+    def _store_uuid(self, uuid):
+        with open(self._uuid_path, 'w') as f:
+            json.dump(uuid, f)
+    
+    def _get_request_body(self, meta):
+
+
+    def _get_settings(self):
+        return self.api.request({
+            "method": "GET",
+            "path": self._api_path
+        })
+
+    def _post_metadata(self, settings_meta):
+        return self.api.request({
+            "method": "POST",
+            "path": self._api_path,
+            "json": settings_meta
+        })
+
+    # account for settings changes through skill ie. change default playlist
+
+    # account for finding a place to store uuid
