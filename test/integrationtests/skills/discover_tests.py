95ce1ed39d1a8fd159cf5b4e839b73db1c55a2df
harika <harika.kpf4@gmail.com>
2017-07-10 17:13:47 -0500

    added some stuffs --arron says it's awesome!

--- test/integrationtests/skills/discover_tests.py
+++ test/integrationtests/skills/discover_tests.py
@@ -2,6 +2,7 @@ import os
 import sys
 import glob
 import unittest
+import json
 from test.integrationtests.skills.skill_tester import MockSkillsLoader
 from test.integrationtests.skills.skill_tester import SkillTest
 
@@ -28,6 +29,12 @@ def discover_tests():
         ]
         if len(test_intent_files) > 0:
             tests[skill] = test_intent_files
+        for file in test_intent_files:
+            output_file = open(file, 'r')
+            output = output_file.read()
+            data_json = json.loads(output)
+            my_dict = {}
+            my_dict = data_json
 
     return tests
 
@@ -39,15 +46,19 @@ class IntentTestSequenceMeta(type):
                 SkillTest(a, b, self.emitter).run(self.loader)
             return test
 
+        print "1: Starting"
         tests = discover_tests()
         for skill in tests.keys():
+            print "2: " + str(skill)
             skill_name = os.path.basename(skill)
             for example in tests[skill]:
                 example_name = os.path.basename(
                     os.path.splitext(os.path.splitext(example)[0])[0])
+                print "3: "+str(example_name)
                 test_name = "test_IntentValidation[%s:%s]" % (skill_name,
                                                               example_name)
                 d[test_name] = gen_test(skill, example)
+        print "3: Finished"
         return type.__new__(mcs, name, bases, d)
 
 
