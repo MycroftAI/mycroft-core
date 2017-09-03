c2aa2ebb97866acbb99ffd3e90b36068e76ffc4a
Brian Hopkins <brianhh1230@gmail.com>
2017-09-03 18:49:08 -0400

    fixing typo (#1052)
    * fixing typo
    
    fixing color function

--- mycroft/client/enclosure/eyes.py
+++ mycroft/client/enclosure/eyes.py
@@ -71,9 +71,9 @@ class EnclosureEyes:
     def color(self, event=None):
         r, g, b = 255, 255, 255
         if event and event.data:
-            r = int(event.data.get("r"), r)
-            g = int(event.data.get("g"), g)
-            b = int(event.data.get("b"), b)
+            r = int(event.data.get("r", r))
+            g = int(event.data.get("g", g))
+            b = int(event.data.get("b", b))
         color = (r * 65536) + (g * 256) + b
         self.writer.write("eyes.color=" + str(color))
 
