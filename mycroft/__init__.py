8f3e0a4095f443cd1f0f7b538fd9d43fdca1aebd
Jarbas <jarbasai@mailfence.com>
2017-09-06 01:25:48 +0100

    fix root path

--- mycroft/__init__.py
+++ mycroft/__init__.py
@@ -1,11 +1,12 @@
+from os.path import abspath, dirname, join
+
+MYCROFT_ROOT_PATH = abspath(join(dirname(__file__), '..'))
+
 from mycroft.api import Api
 from mycroft.skills.core import MycroftSkill, FallbackSkill, \
     intent_handler, intent_file_handler
 from mycroft.skills.context import adds_context, removes_context
 from mycroft.messagebus.message import Message
 
-from os.path import abspath, dirname, join
 
 __author__ = 'seanfitz'
-
-MYCROFT_ROOT_PATH = abspath(join(dirname(__file__), '..'))
