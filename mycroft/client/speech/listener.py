3eec8f73e950208fd55c9e52c7c4515984b50736
jarbasai <jarbasai@mailfence.com>
2017-09-15 01:36:00 +0100

    bug fix

--- mycroft/client/speech/listener.py
+++ mycroft/client/speech/listener.py
@@ -221,7 +221,7 @@ class RecognizerLoop(EventEmitter):
     def create_wake_word_recognizer(self):
         # Create a local recognizer to hear the wakeup word, e.g. 'Hey Mycroft'
         LOG.info("creating wake word engine")
-        word = "hey mycroft"  # self.config.get("wake_word", "hey mycroft")
+        word = self.config.get("wake_word", "hey mycroft")
         # TODO remove this, only for server settings compatibility
         phonemes = self.config.get("phonemes")
         thresh = self.config.get("threshold")
