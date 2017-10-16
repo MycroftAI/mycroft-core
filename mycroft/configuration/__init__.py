91c790f1e67f88a0eafbe3a2f8356074f6d89808
Åke Forslund <ake.forslund@gmail.com>
2017-10-16 16:52:33 +0200

    Fix PEP-8

--- mycroft/configuration/__init__.py
+++ mycroft/configuration/__init__.py
@@ -12,9 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from config import Configuration
-ConfigurationManager = Configuration
-
 from config import Configuration, LocalConf, RemoteConf, \
                    SYSTEM_CONFIG, USER_CONFIG
 
