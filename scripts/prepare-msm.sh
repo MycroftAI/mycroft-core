89c841e98f84b2af40a147169d48cf7798ee29e8
Åke Forslund <ake.forslund@gmail.com>
2018-01-05 08:32:35 +0100

    Add support for installing on openSuse
    - Use Zypper to install packages
    - use group of current user when changing owner of /opt/mycroft

--- scripts/prepare-msm.sh
+++ scripts/prepare-msm.sh
@@ -31,11 +31,12 @@ chmod +x ${DIR}/../msm/msm
 
 # Determine which user is running this script
 setup_user=$USER
+setup_group=$( id -gn $USER )
 
 # change ownership of ${mycroft_root_dir} to ${setup_user } recursively 
 function change_ownership {
-    echo "Changing ownership of" ${mycroft_root_dir} "to user:" ${setup_user} "with group:" ${setup_user}
-            sudo chown -Rvf ${setup_user}:${setup_user} ${mycroft_root_dir}
+    echo "Changing ownership of" ${mycroft_root_dir} "to user:" ${setup_user} "with group:" ${setup_group}
+            sudo chown -Rvf ${setup_user}:${setup_group} ${mycroft_root_dir}
 }
 
 
