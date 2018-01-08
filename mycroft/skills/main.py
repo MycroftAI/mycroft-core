1b819b1a4d35a65b7f492ef72fe2783dcee2171e
jarbasai <jarbasai@mailfence.com>
2018-01-08 04:25:36 +0000

    add skill load fail message

--- mycroft/skills/main.py
+++ mycroft/skills/main.py
@@ -302,16 +302,16 @@ class SkillManager(Thread):
                                            self.ws, skill["id"],
                                            BLACKLISTED_SKILLS)
             skill["last_modified"] = modified
-            if skill and skill['instance']:
-                ws.emit(Message('mycroft.skills.loaded',
-                                {'id': skill['id'],
-                                 'name': skill['instance'].name}))
-
-            self.ws.emit(Message("skill.loaded",
-                                 {'folder': skill_folder,
-                                  "id": skill["id"],
-                                  "name": skill["instance"].name,
-                                  "modified": modified}))
+            if skill["instance"] is not None:
+                self.ws.emit(Message("mycroft.skills.loaded",
+                                     {'folder': skill_folder,
+                                        "id": skill["id"],
+                                        "name": skill["instance"].name,
+                                        "modified": modified}))
+            else:
+                self.ws.emit(Message("mycroft.skills.loading_failure",
+                                     {'folder': skill_folder,
+                                      "id": skill["id"]}))
 
     def load_skill_list(self, skills_to_load):
         """ Load the specified list of skills from disk
