98960012bb38c49281988d1534e16194e2b5d07c
jarbasai <jarbasai@mailfence.com>
2018-01-08 04:25:36 +0000

    add skill load fail message

--- mycroft/skills/main.py
+++ mycroft/skills/main.py
@@ -39,6 +39,7 @@ from mycroft.skills.padatious_service import PadatiousService
 from mycroft.util import connected, wait_while_speaking
 from mycroft.util.log import LOG
 
+
 ws = None
 event_scheduler = None
 skill_manager = None
@@ -194,7 +195,7 @@ class SkillManager(Thread):
         super(SkillManager, self).__init__()
         self._stop_event = Event()
         self._loaded_priority = Event()
-        self.next_download = time.time() - 1  # download ASAP
+        self.next_download = time.time() - 1    # download ASAP
         self.loaded_skills = {}
         self.msm_blocked = False
         self.ws = ws
@@ -264,11 +265,8 @@ class SkillManager(Thread):
                     self.next_download = time.time() + 60 * MINUTES
 
                     if res == 0 and speak:
-                        self.ws.emit(
-                            Message("speak",
-                                    {'utterance':
-                                     mycroft.dialog.get("skills updated")
-                                     }))
+                        self.ws.emit(Message("speak", {'utterance':
+                                     mycroft.dialog.get("skills updated")}))
                     return True
                 elif not connected():
                     LOG.error('msm failed, network connection not available')
@@ -335,9 +333,12 @@ class SkillManager(Thread):
                     LOG.warning(
                         "After shutdown of {} there are still "
                         "{} references remaining. The skill "
-                        "won't be cleaned from memory.".format(
-                            skill['instance'].name, refs))
+                        "won't be cleaned from memory."
+                        .format(skill['instance'].name, refs))
             del skill["instance"]
+            self.ws.emit(Message("mycroft.skills.shutdown",
+                                 {'folder': skill_folder,
+                                  "id": skill["id"]}))
 
         # (Re)load the skill from disk
         with self.__msm_lock:  # Make sure msm isn't running
@@ -347,16 +348,16 @@ class SkillManager(Thread):
                                            self.ws, skill["id"],
                                            BLACKLISTED_SKILLS)
             skill["last_modified"] = modified
-            if skill and skill['instance']:
-                ws.emit(Message('mycroft.skills.loaded',
-                                {'id': skill['id'],
-                                 'name': skill['instance'].name}))
-
-            self.ws.emit(Message("skill.loaded",
-                                 {'folder': skill_folder,
-                                  "id": skill["id"],
-                                  "name": skill["instance"].name,
-                                  "modified": modified}))
+            if skill["instance"] is not None:
+                self.ws.emit(Message("mycroft.skills.loaded",
+                                     {'folder': skill_folder,
+                                      "id": skill["id"],
+                                      "name": skill["instance"].name,
+                                      "modified": modified}))
+            else:
+                self.ws.emit(Message("mycroft.skills.loading_failure",
+                                     {'folder': skill_folder,
+                                      "id": skill["id"]}))
 
     def load_skill_list(self, skills_to_load):
         """ Load the specified list of skills from disk
