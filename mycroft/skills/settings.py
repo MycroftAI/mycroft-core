67d3a7b4ebfb93f6baf7bc5cae01f29e9ba20803
Åke Forslund <ake.forslund@gmail.com>
2018-02-09 09:32:43 +0100

    Use sorted json to perform hash of settings

--- mycroft/skills/settings.py
+++ mycroft/skills/settings.py
@@ -94,7 +94,7 @@ class SkillSettings(dict):
         self._settings_path = join(directory, 'settings.json')
         self._meta_path = join(directory, 'settingsmeta.json')
         self.is_alive = True
-        self.loaded_hash = hash(frozenset(self.items()))
+        self.loaded_hash = hash(json.dumps(self, sort_keys=True))
         self._complete_intialization = False
         self._device_identity = None
         self._api_path = None
@@ -162,7 +162,7 @@ class SkillSettings(dict):
 
     @property
     def _is_stored(self):
-        return hash(bytes(str(self), 'utf-8')) == self.loaded_hash
+        return hash(json.dumps(self, sort_keys=True)) == self.loaded_hash
 
     def __getitem__(self, key):
         """ Get key """
@@ -538,7 +538,7 @@ class SkillSettings(dict):
         if force or not self._is_stored:
             with open(self._settings_path, 'w') as f:
                 json.dump(self, f)
-            self.loaded_hash = hash(bytes(str((self)), 'utf-8'))
+            self.loaded_hash = hash(json.dumps(self, sort_keys=True))
 
         if self._should_upload_from_change:
             settings_meta = self._load_settings_meta()
