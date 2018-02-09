0b0d31e99aa0f40a54249ed487b2dea0dcc6fb75
Åke Forslund <ake.forslund@gmail.com>
2018-02-09 09:47:11 +0100

    Fix dev_setup issues
    - venv is CAN be setup in custom directory
    - restore package installation
    - set correct package for python3-venv

--- dev_setup.sh
+++ dev_setup.sh
@@ -91,7 +91,7 @@ install_deps() {
     fi
 
     if found_exe apt-get; then
-        $SUDO apt-get install -y git python python-dev python-setuptools python-venv python-gobject-dev libtool libffi-dev libssl-dev autoconf automake bison swig libglib2.0-dev s3cmd portaudio19-dev mpg123 screen flac curl libicu-dev pkg-config automake libjpeg-dev libfann-dev build-essential jq
+        $SUDO apt-get install -y git python python-dev python-setuptools python-gobject-dev libtool libffi-dev libssl-dev autoconf automake bison swig libglib2.0-dev s3cmd portaudio19-dev mpg123 screen flac curl libicu-dev pkg-config automake libjpeg-dev libfann-dev build-essential jq
     elif found_exe pacman; then
         $SUDO pacman -S --needed git python2 python2-pip python2-setuptools python2-virtualenv python2-gobject python-virtualenvwrapper libtool libffi openssl autoconf bison swig glib2 s3cmd portaudio mpg123 screen flac curl pkg-config icu automake libjpeg-turbo base-devel jq
     elif found_exe dnf; then
@@ -113,10 +113,11 @@ TOP=$(cd $(dirname $0) && pwd -L)
 VIRTUALENV_ROOT=${VIRTUALENV_ROOT:-"${TOP}/.venv"}
 
 install_venv() {
-    python3 -m venv .venv/
-    curl https://bootstrap.pypa.io/get-pip.py | .venv/bin/python
+    python3 -m venv "${VIRTUALENV_ROOT}/"
+    curl https://bootstrap.pypa.io/get-pip.py | "${VIRTUALENV_ROOT}/bin/python"
 }
-#install_deps
+
+install_deps
 
 # Configure to use the standard commit template for
 # this repo only.
