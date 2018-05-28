e7a6a2096717551bbcb186978733281a86a88f18
Åke Forslund <ake.forslund@gmail.com>
2018-05-28 15:32:56 +0200

    Sort test cases so order is enforced

--- test/integrationtests/skills/discover_tests.py
+++ test/integrationtests/skills/discover_tests.py
@@ -38,7 +38,7 @@ def discover_tests(skills_dir):
     tests = {}
     skills = [
         skill for skill
-        in glob.glob(skills_dir + '/*')
+        in sorted(glob.glob(skills_dir + '/*'))
         if os.path.isdir(skill)
     ]
 
@@ -54,7 +54,8 @@ def discover_tests(skills_dir):
         # Find all intent test files
         test_intent_files = [
             (f, test_env) for f
-            in glob.glob(os.path.join(skill, 'test/intent/*.intent.json'))
+            in sorted(
+                glob.glob(os.path.join(skill, 'test/intent/*.intent.json')))
         ]
         if len(test_intent_files) > 0:
             tests[skill] = test_intent_files
