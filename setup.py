a14a8b6d7e797b7737d85c7a454317ef310d75d8
Åke Forslund <ake.forslund@gmail.com>
2018-07-14 17:08:54 +0200

    Remove setup.py's reliance on the mycroft modules
    - Adds missing fields, such as description, author and e-mail
    - Removes the old setup_base.py from the mycroft.util module

--- setup.py
+++ setup.py
@@ -12,22 +12,52 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from setuptools import setup
+from setuptools import setup, find_packages
+import os.path
 
-from mycroft.util.setup_base import (
-    find_all_packages,
-    required,
-    get_version,
-    place_manifest
-)
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
-place_manifest('mycroft-base-MANIFEST.in')
+
+def get_version():
+    """ Find the version of mycroft-core"""
+    version = None
+    version_file = os.path.join(BASEDIR, 'mycroft', 'version', '__init__.py')
+    major, minor, build = (None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'CORE_VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'CORE_VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'CORE_VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+
+            if ((major and minor and build) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = '.'.join([major, minor, build])
+
+    return version
+
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
 
 setup(
-    name="mycroft-core",
+    name='mycroft-core',
     version=get_version(),
-    install_requires=[required('requirements.txt')],
-    packages=find_all_packages("mycroft"),
+    license='Apache-2.0',
+    author='Mycroft A.I.',
+    author_email='devs@mycroft.ai',
+    url='https://github.com/MycroftAI/mycroft-core',
+    description='Mycroft Core',
+    install_requires=required('requirements.txt'),
+    packages=find_packages(include=['mycroft*']),
     include_package_data=True,
 
     entry_points={
