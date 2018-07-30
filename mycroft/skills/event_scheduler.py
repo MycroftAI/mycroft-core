ec73b7d48e785d56ee2270d8dcc9b998cf82dfad
Steve Penrod <github@gpll.org>
2018-07-30 15:08:13 -0500

    Fix named event scheduling/deleting (#1705)
    While working on the Alarm skill I discovered several issues with the
    event scheduler.  This PR cleans up those findings and resolves several
    other potential issues:
    
    1) To avoid thread synchronization issues, the EventScheduler had several
    queues which independently held objects to be added/deleted/updated.  However, the order of the events was undefined and got mixed since they were all batched together.  So, for instance, if skill code performed:
       self.add_event("foo", self.handle_foo)
       if SomeReason:
           self.cancel_event("foo")
    The actual order of queue handling would perform Remove first, then Add which resulted in "foo" not being found for delete, but then added and left as an active event.
    
    Now the EventScheduler protects the list using a Lock and the queues have been removed.  Modifications to the list happen immediately after obtaining the lock and are not batched up.
    
    2) One-time events were triggered while the event was still in the EventScheduler list.  Now the entry is removed before invoking the handler.
    
    3) Within the MycroftSkill.add_event(name, handler) is a local 'wrapper' method that actually makes the callback.  The MycroftSkill.remove_event(name) method attempted to find entries in the events list and the associated handler entries in the self.emitter to remove.  However, the emitter actually held the wrapper(handler), not the handler itself.  So the emitter handlers were left behind.
    
    This was a quiet bug until the next time you scheduled an event of the same name.  When that second event finally triggered, it would fire off both the new and the old handler -- which snowballed in the 'skill-alarm:Beep' case, doubling and redoubling with every beep.
    
    Now this cancels all the emitter listeners by name.  There is a very slim chance that someone has registered a listener with the same name, but since it is namespaced to "skill-name:Event" I think this is pretty safe.
    
    
    Not technically related, but a failure that has been lurking for
    some time and is a French unit test that doesn't work depending
    on the time of day when the test is run.

--- mycroft/skills/event_scheduler.py
+++ mycroft/skills/event_scheduler.py
@@ -14,19 +14,18 @@
 #
 import json
 import time
-from threading import Thread
+from threading import Thread, Lock
 
 from os.path import isfile, join, expanduser
 
 from mycroft.configuration import Configuration
 from mycroft.messagebus.message import Message
 from mycroft.util.log import LOG
-from queue import Queue
 
 
 def repeat_time(sched_time, repeat):
-    """Next scheduled time for repeating event. Asserts that the
-    time is not in the past.
+    """Next scheduled time for repeating event. Guarantees that the
+    time is not in the past (but could skip interim events)
 
     Args:
         sched_time (float): Scheduled unix time for the event
@@ -35,9 +34,9 @@ def repeat_time(sched_time, repeat):
     Returns: (float) time for next event
     """
     next_time = sched_time + repeat
-    if next_time < time.time():
+    while next_time < time.time():
         # Schedule at an offset to assure no doubles
-        next_time = time.time() + repeat
+        next_time = time.time() + abs(repeat)
     return next_time
 
 
@@ -55,15 +54,14 @@ class EventScheduler(Thread):
         data_dir = expanduser(Configuration.get()['data_dir'])
 
         self.events = {}
+        self.event_lock = Lock()
+
         self.emitter = emitter
         self.isRunning = True
         self.schedule_file = join(data_dir, schedule_file)
         if self.schedule_file:
             self.load()
 
-        self.add = Queue()
-        self.remove = Queue()
-        self.update = Queue()
         self.emitter.on('mycroft.scheduler.schedule_event',
                         self.schedule_event_handler)
         self.emitter.on('mycroft.scheduler.remove_event',
@@ -86,18 +84,60 @@ class EventScheduler(Thread):
                 except Exception as e:
                     LOG.error(e)
             current_time = time.time()
-            for key in json_data:
-                event_list = json_data[key]
-                # discard non repeating events that has already happened
-                self.events[key] = [tuple(e) for e in event_list
-                                    if e[0] > current_time or e[1]]
+            with self.event_lock:
+                for key in json_data:
+                    event_list = json_data[key]
+                    # discard non repeating events that has already happened
+                    self.events[key] = [tuple(e) for e in event_list
+                                        if e[0] > current_time or e[1]]
 
-    def fetch_new_events(self):
+    def run(self):
+        while self.isRunning:
+            self.check_state()
+            time.sleep(0.5)
+
+    def check_state(self):
         """
-            Fetch new events and add to list of pending events.
+            Check if an event should be triggered.
         """
-        while not self.add.empty():
-            event, sched_time, repeat, data = self.add.get(timeout=1)
+        with self.event_lock:
+            # Check all events
+            pending_messages = []
+            for event in self.events:
+                current_time = time.time()
+                e = self.events[event]
+                # Get scheduled times that has passed
+                passed = [(t, r, d) for (t, r, d) in e if t <= current_time]
+                # and remaining times that we're still waiting for
+                remaining = [(t, r, d) for t, r, d in e if t > current_time]
+                # Trigger registered methods
+                for sched_time, repeat, data in passed:
+                    pending_messages.append(Message(event, data))
+                    # if this is a repeated event add a new trigger time
+                    if repeat:
+                        next_time = repeat_time(sched_time, repeat)
+                        remaining.append((next_time, repeat, data))
+                # update list of events
+                self.events[event] = remaining
+
+        # Remove events have are now completed
+        self.clear_empty()
+
+        # Finally, emit the queued up events that triggered
+        for msg in pending_messages:
+            self.emitter.emit(msg)
+
+    def schedule_event(self, event, sched_time, repeat=None, data=None):
+        """ Add event to pending event schedule using thread safe queue.
+
+        Args:
+            event (str): Handler for the event
+            sched_time ([type]): [description]
+            repeat ([type], optional): Defaults to None. [description]
+            data ([type], optional): Defaults to None. [description]
+        """
+        data = data or {}
+        with self.event_lock:
             # get current list of scheduled times for event, [] if missing
             event_list = self.events.get(event, [])
 
@@ -109,65 +149,6 @@ class EventScheduler(Thread):
                 # add received event and time
                 event_list.append((sched_time, repeat, data))
                 self.events[event] = event_list
-
-    def remove_events(self):
-        """
-            Remove event from event list.
-        """
-        while not self.remove.empty():
-            event = self.remove.get()
-            if event in self.events:
-                self.events.pop(event)
-
-    def update_events(self):
-        """
-            Update event list with new data.
-        """
-        while not self.remove.empty():
-            event, data = self.update.get()
-            # if there is an active event with this name
-            if len(self.events.get(event, [])) > 0:
-                time, repeat, _ = self.events[event][0]
-                self.events[event][0] = (time, repeat, data)
-
-    def run(self):
-        while self.isRunning:
-            self.check_state()
-            time.sleep(0.5)
-
-    def check_state(self):
-        """
-            Check if an event should be triggered.
-        """
-        # Remove events
-        self.remove_events()
-        # Fetch newly scheduled events
-        self.fetch_new_events()
-        # Update events
-        self.update_events()
-
-        # Check all events
-        for event in self.events:
-            current_time = time.time()
-            e = self.events[event]
-            # Get scheduled times that has passed
-            passed = [(t, r, d) for (t, r, d) in e if t <= current_time]
-            # and remaining times that we're still waiting for
-            remaining = [(t, r, d) for t, r, d in e if t > current_time]
-            # Trigger registered methods
-            for sched_time, repeat, data in passed:
-                self.emitter.emit(Message(event, data))
-                # if this is a repeated event add a new trigger time
-                if repeat:
-                    next_time = repeat_time(sched_time, repeat)
-                    remaining.append((next_time, repeat, data))
-            # update list of events
-            self.events[event] = remaining
-
-    def schedule_event(self, event, sched_time, repeat=None, data=None):
-        """ Send event to thread using thread safe queue. """
-        data = data or {}
-        self.add.put((event, sched_time, repeat, data))
 
     def schedule_event_handler(self, message):
         """
@@ -193,8 +174,9 @@ class EventScheduler(Thread):
             LOG.error('Scheduled event time not provided')
 
     def remove_event(self, event):
-        """ Remove event using thread safe queue. """
-        self.remove.put(event)
+        with self.event_lock:
+            if event in self.events:
+                self.events.pop(event)
 
     def remove_event_handler(self, message):
         """ Messagebus interface to the remove_event method. """
@@ -202,7 +184,11 @@ class EventScheduler(Thread):
         self.remove_event(event)
 
     def update_event(self, event, data):
-        self.update((event, data))
+        with self.event_lock:
+            # if there is an active event with this name
+            if len(self.events.get(event, [])) > 0:
+                time, repeat, _ = self.events[event][0]
+                self.events[event][0] = (time, repeat, data)
 
     def update_event_handler(self, message):
         """ Messagebus interface to the update_event method. """
@@ -217,8 +203,9 @@ class EventScheduler(Thread):
         """
         event_name = message.data.get("name")
         event = None
-        if event_name in self.events:
-            event = self.events[event_name]
+        with self.event_lock:
+            if event_name in self.events:
+                event = self.events[event_name]
         emitter_name = 'mycroft.event_status.callback.{}'.format(event_name)
         self.emitter.emit(message.reply(emitter_name, data=event))
 
@@ -226,22 +213,25 @@ class EventScheduler(Thread):
         """
             Write current schedule to disk.
         """
-        with open(self.schedule_file, 'w') as f:
-            json.dump(self.events, f)
+        with self.event_lock:
+            with open(self.schedule_file, 'w') as f:
+                json.dump(self.events, f)
 
     def clear_repeating(self):
         """
             Remove repeating events from events dict.
         """
-        for e in self.events:
-            self.events[e] = [i for i in self.events[e] if i[1] is None]
+        with self.event_lock:
+            for e in self.events:
+                self.events[e] = [i for i in self.events[e] if i[1] is None]
 
     def clear_empty(self):
         """
             Remove empty event entries from events dict
         """
-        self.events = {k: self.events[k] for k in self.events
-                       if self.events[k] != []}
+        with self.event_lock:
+            self.events = {k: self.events[k] for k in self.events
+                           if self.events[k] != []}
 
     def shutdown(self):
         """ Stop the running thread. """
