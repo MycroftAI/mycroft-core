684230213edf31fb5ff4682e5df69568b640c091
Steve Penrod <steve.penrod@mycroft.ai>
2018-08-15 03:50:37 -0500

    Set the directory explicitly start-mycroft.sh scripts
    Explicitly setting the directory at the beginning of the start-mycroft.sh
    and stop-mycroft.sh scripts.  The behaves better if invoked from a
    different directory.

--- stop-mycroft.sh
+++ stop-mycroft.sh
@@ -18,7 +18,8 @@ SOURCE="${BASH_SOURCE[0]}"
 
 script=${0}
 script=${script##*/}
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+cd -P "$( dirname "$SOURCE" )"
+DIR="$( pwd )"
 
 function help() {
     echo "${script}:  Mycroft service stopper"
