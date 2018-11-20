c443f1dd3239dd1734066c31dcb26738c337fb77
jarbasal <jarbasai@mailfence.com>
2018-11-20 08:18:44 +0000

    fix converse

--- mycroft/skills/skill_manager.py
+++ mycroft/skills/skill_manager.py
@@ -534,8 +534,7 @@ class SkillManager(Thread):
 
         # loop trough skills list and call converse for skill with skill_id
         for skill in self.loaded_skills:
-            if (self.loaded_skills[skill]["instance"] and
-                    self.loaded_skills[skill]["id"] == skill_id):
+            if self.loaded_skills[skill]["id"] == skill_id:
                 try:
                     instance = self.loaded_skills[skill]["instance"]
                 except BaseException:
