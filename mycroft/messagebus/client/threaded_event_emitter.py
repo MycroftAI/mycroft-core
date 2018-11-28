5ed2a872dce486215f6486e9a338395f0c676e2b
Åke Forslund <ake.forslund@gmail.com>
2018-11-28 15:43:07 +0100

    Run emitter using threadpool
    This moves the thread pool from the websocket client into the eventemitter allowing each registered function to run in a separate thread and not just each event.
    
    This speeds up cases where there is a one to many call such as the common play framework and the upcomming common query framework.

---
+++ mycroft/messagebus/client/threaded_event_emitter.py
@@ -0,0 +1,43 @@
+from pyee import EventEmitter
+from multiprocessing.pool import ThreadPool
+from collections import defaultdict
+
+
+class ThreadedEventEmitter(EventEmitter):
+    """ Event Emitter using the threadpool to run event functions in
+        separate threads.
+    """
+    def __init__(self, threads=10):
+        super().__init__()
+        self.pool = ThreadPool(threads)
+        self.wrappers = defaultdict(list)
+
+    def on(self, event, f=None):
+        """ Wrap on with a threaded launcher. """
+        def wrapped(*args, **kwargs):
+            return self.pool.apply_async(f, args, kwargs)
+
+        w = super().on(event, wrapped)
+        # Store mapping from function to wrapped function
+        self.wrappers[event].append((f, wrapped))
+        return w
+
+    def once(self, event, f=None):
+        """ Wrap once with a threaded launcher. """
+        def wrapped(*args, **kwargs):
+            return self.pool.apply_async(f, args, kwargs)
+
+        wrapped = super().once(event, wrapped)
+        self.wrappers[event].append((f, wrapped))
+        return wrapped
+
+    def remove_listener(self, event_name, func):
+        """ Wrap the remove to translate from function to wrapped
+            function.
+        """
+        for w in self.wrappers[event_name]:
+            if w[0] == func:
+                self.wrappers[event_name].remove(w)
+                return super().remove_listener(event_name, w[1])
+        # if no wrapper exists try removing the function
+        return super().remove_listener(event_name, func)
