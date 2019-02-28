07c2a93324244ba580515e5dd62bfea143429154
Åke <ake.forslund@gmail.com>
2019-02-28 10:04:51 +0100

    Feature/threaded emitter for real (#2024)
    * Run emitter using threadpool
    
    This moves the thread pool from the websocket client into the eventemitter allowing each registered function to run in a separate thread and not just each event.
    
    This speeds up cases where there is a one to many call such as the common play framework and the upcomming common query framework.
    
    * Add unit tests for threaded event emitter
    
    * Add standard header
    
    * Add standard header

---
+++ test/unittests/messagebus/client/test_threaded_event_emitter.py
@@ -0,0 +1,58 @@
+# Copyright 2019 Mycroft AI Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from time import sleep
+
+from mycroft import Message
+from mycroft.messagebus.client.threaded_event_emitter import \
+    ThreadedEventEmitter
+
+
+class TestThreadedEventEmitter:
+    def setup(self):
+        self.emitter = ThreadedEventEmitter()
+        self.count = 0
+        self.msg = Message('testing')
+
+    def example_event(self, message):
+        self.count += 1
+
+    def test_on(self):
+        self.emitter.on(self.msg.type, self.example_event)
+        self.emitter.emit(self.msg.type, self.msg)
+        self.emitter.emit(self.msg.type, self.msg)
+        sleep(0.1)
+        assert self.count == 2
+
+    def test_once(self):
+        self.emitter.once(self.msg.type, self.example_event)
+        self.emitter.emit(self.msg.type, self.msg)
+        self.emitter.emit(self.msg.type, self.msg)
+        sleep(0.1)
+        assert self.count == 1
+
+    def test_remove_listener_on(self):
+        self.emitter.on(self.msg.type, self.example_event)
+        self.emitter.remove_listener(self.msg.type, self.example_event)
+        self.emitter.emit(self.msg.type)
+        sleep(0.1)
+        assert self.count == 0
+
+    def test_remove_all_listeners(self):
+        self.emitter.on(self.msg.type, self.example_event)
+        self.emitter.once(self.msg.type, self.example_event)
+        self.emitter.remove_all_listeners(self.msg.type)
+        self.emitter.emit(self.msg.type)
+        sleep(0.1)
+        assert self.count == 0
