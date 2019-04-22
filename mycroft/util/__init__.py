32636fe55f94a1ac814faad171069bc642acc33a
Åke <ake.forslund@gmail.com>
2019-04-22 21:06:01 +0200

    Set default log level to INFO (#2070)
    * Set default log level to INFO
    
    * Restore echo functionality
    
    - Make sure that the mycroft.debug.log message isn't removed by the
    whitelist
    - whitelist now only checks the beginning of a message so a whitelisted
    "mycroft.audio.service" will allow messages such as
    "mycroft.audio.service.play"
    
    * Make MycroftSkill.log track root logger
    
    modify root logger when changing log level will now change the log level
    of the skill logger.
    
    * Changes for debugging
    
    * Switched bus logging to use INFO instead of DEBUG so it always displays when turned on
    * Add exception output
    * Replaced multiple calls into a msg_type variable

--- mycroft/util/__init__.py
+++ mycroft/util/__init__.py
@@ -465,43 +465,54 @@ def create_echo_function(name, whitelist=None):
     from mycroft.configuration import Configuration
     blacklist = Configuration.get().get("ignore_logs")
 
+    # Make sure whitelisting doesn't remove the log level setting command
+    if whitelist:
+        whitelist.append('mycroft.debug.log')
+
     def echo(message):
         global _log_all_bus_messages
         try:
             msg = json.loads(message)
-
-            if whitelist and msg.get("type") not in whitelist:
+            msg_type = msg.get("type", "")
+            # Whitelist match beginning of message
+            # i.e 'mycroft.audio.service' will allow the message
+            # 'mycroft.audio.service.play' for example
+            if whitelist and not any([msg_type.startswith(e)
+                                     for e in whitelist]):
                 return
 
-            if blacklist and msg.get("type") in blacklist:
+            if blacklist and msg_type in blacklist:
                 return
 
-            if msg.get("type") == "mycroft.debug.log":
+            if msg_type == "mycroft.debug.log":
                 # Respond to requests to adjust the logger settings
                 lvl = msg["data"].get("level", "").upper()
                 if lvl in ["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"]:
                     LOG.level = lvl
                     LOG(name).info("Changing log level to: {}".format(lvl))
                     try:
+                        logging.getLogger().setLevel(lvl)
                         logging.getLogger('urllib3').setLevel(lvl)
                     except Exception:
                         pass  # We don't really care about if this fails...
+                else:
+                    LOG(name).info("Invalid level provided: {}".format(lvl))
 
                 # Allow enable/disable of messagebus traffic
                 log_bus = msg["data"].get("bus", None)
                 if log_bus is not None:
-                    LOG(name).info("Bus logging: "+str(log_bus))
+                    LOG(name).info("Bus logging: {}".format(log_bus))
                     _log_all_bus_messages = log_bus
-            elif msg.get("type") == "registration":
+            elif msg_type == "registration":
                 # do not log tokens from registration messages
                 msg["data"]["token"] = None
                 message = json.dumps(msg)
-        except Exception:
-            pass
+        except Exception as e:
+            LOG.info("Error: {}".format(repr(e)), exc_info=True)
 
         if _log_all_bus_messages:
             # Listen for messages and echo them for logging
-            LOG(name).debug(message)
+            LOG(name).info("BUS: {}".format(message))
     return echo
 
 
