7ba3889054032b89b9eff2e2a87e04e8b582d407
Aditya Mehra <Aix.m@outlook.com>
2019-04-23 16:48:07 +0530

    Enable sending qml ui over network

--- mycroft/skills/core.py
+++ mycroft/skills/core.py
@@ -234,6 +234,11 @@ class SkillGUI:
         self.page = None    # the active GUI page (e.g. QML template) to show
         self.skill = skill
         self.on_gui_changed_callback = None
+        self.config_core = Configuration.get()
+        
+    @property
+    def remote_url(self):
+        return self.config_core.get('remote-server')
 
     def build_message_type(self, event):
         """ Builds a message matching the output from the enclosure. """
@@ -346,7 +351,10 @@ class SkillGUI:
         for name in page_names:
             page = self.skill.find_resource(name, 'ui')
             if page:
-                page_urls.append("file://" + page)
+                if self.config_core.get('remote') is True:
+                    page_urls.append(self.remote_url + "/" + page)
+                else:
+                    page_urls.append("file://" + page)
             else:
                 raise FileNotFoundError("Unable to find page: {}".format(name))
 
