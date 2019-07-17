be3dff99a1cd54605c012367a9a583df6eb0753e
Chris Veilleux <veilleux.chris@gmail.com>
2019-07-17 10:21:51 -0500

    added licence info to files without it and a missing docstring

--- mycroft/messagebus/load_config.py
+++ mycroft/messagebus/load_config.py
@@ -1,3 +1,17 @@
+# Copyright 2019 Mycroft AI Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 from collections import namedtuple
 
 from mycroft.configuration import Configuration
@@ -10,6 +24,7 @@ MessageBusConfig = namedtuple(
 
 
 def load_message_bus_config(**overrides):
+    """Load the bits of device configuration needed to run the message bus."""
     LOG.info('Loading message bus configs')
     config = Configuration.get()
 
