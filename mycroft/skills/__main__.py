91e70b6fd15e581af9c7125171fa59fcbc825785
Chris Veilleux <veilleux.chris@gmail.com>
2019-07-23 13:31:06 -0500

    applied some code structure changes after reviewing code with Ake.

--- mycroft/skills/__main__.py
+++ mycroft/skills/__main__.py
@@ -19,7 +19,7 @@ directory.  The executable gets added to the bin directory when installed
 (see setup.py)
 """
 import time
-from threading import Timer
+from threading import Event
 
 from mycroft import dialog
 from mycroft.api import is_paired, BackendDown, DeviceApi
@@ -38,7 +38,6 @@ from mycroft.util import (
 )
 from mycroft.util.lang import set_active_lang
 from mycroft.util.log import LOG
-
 from .core import FallbackSkill
 from .event_scheduler import EventScheduler
 from .intent_service import IntentService
@@ -48,100 +47,44 @@ from .skill_manager import SkillManager, MsmException
 RASPBERRY_PI_PLATFORMS = ('mycroft_mark_1', 'picroft', 'mycroft_mark_2pi')
 ONE_HOUR = 3600
 
-bus = None  # Mycroft message bus reference, see "mycroft.messagebus"
-skill_service = None
-
-# Remember "now" at startup.  Used to detect clock changes.
-start_ticks = time.monotonic()
-start_clock = time.time()
-
-
-class SkillService(object):
-    def __init__(self, message_bus_client, config):
-        self.bus = message_bus_client
-        self.config = config
-        self._define_message_bus_handlers()
-        self.event_scheduler = None
-        self.skill_manager = None
-
-    def _define_message_bus_handlers(self):
-        self.bus.on('message', create_echo_function('SKILLS'))
-        self.bus.on(
-            'intent_failure',
-            FallbackSkill.make_intent_failure_handler(self.bus)
-        )
-
-    def start(self):
-        self._start_intent_services()
-        self.event_scheduler = EventScheduler(self.bus)
-        self._start_skill_manager()
-
-    def _start_intent_services(self):
-        """Instantiate the Adapt and Padatious intent managers.
-
-        The intent managers convert utterances to intents, which used to invoke
-        the skill system.  Skill handlers define intents that, when matched,
-        will execute the code within the skill handler.
-
-        When no match is found for the intent, an intent_failu re message is
-        emitted, which invokes the fallback mechanism.
-        """
-        LOG.info('Starting Adapt and Padatious intent services...')
-        service = IntentService(self.bus)  # Adapt
-        try:
-            PadatiousService(self.bus, service)
-        except Exception as e:
-            LOG.error('Failed to create Padatious intent handlers')
-            LOG.exception(e)
-
-    def _start_skill_manager(self):
-        """Create a thread that monitors loaded skills looking for updates."""
-        LOG.info('Starting the skill manager...')
-        # TODO: if this needs an internet connection, don't start it so soon
-        try:
-            self.skill_manager = SkillManager(self.bus)
-        except MsmException:
-            # skill manager couldn't be created, wait for network connection
-            # and retry
-            LOG.error(
-                'Msm is uninitialized and requires network connection to fetch'
-                'skill information\n Waiting for network connection...'
-            )
-            while not connected():
-                time.sleep(30)
-            self.skill_manager = SkillManager(self.bus)
-
-        self.skill_manager.daemon = True
-
-        # Wait until priority skills have been loaded before checking
-        # network connection
-        self.skill_manager.load_priority()
-        self.skill_manager.start()
-
 
 class DevicePrimer(object):
     def __init__(self, message_bus_client, config):
         self.bus = message_bus_client
         self.platform = config['enclosure'].get("platform", "unknown")
-        self.enclosure = None
+        self.enclosure = EnclosureAPI(self.bus)
         self.is_paired = False
         self.backend_down = False
+        # Remember "now" at startup.  Used to detect clock changes.
+        self.start_ticks = time.monotonic()
+        self.start_clock = time.time()
+
+    @property
+    def time_skew_threshold_exceeded(self):
+        """Did the NTP sync skew system time significantly?"""
+        skew = abs(
+            (time.monotonic() - self.start_ticks) -
+            (time.time() - self.start_clock)
+        )
+
+        return skew > ONE_HOUR
 
     def prepare_device(self):
         """Internet dependent updates of various aspects of the device."""
-        self.enclosure = EnclosureAPI(self.bus)
         self._get_pairing_status()
         self._update_system_clock()
-        self._attempt_system_update()
-        reboot = self._check_time_skew()
-        if not reboot:
-            if self.backend_down:
-                self._notify_backend_down()
+        update_attempted = self._update_system()
+        if not update_attempted:
+            if self.time_skew_threshold_exceeded:
+                self._reboot()
             else:
-                self._display_skill_loading_notification()
-                self.bus.emit(Message('mycroft.internet.connected'))
-                self._ensure_device_is_paired()
-                self._update_device_attributes_on_backend()
+                if self.backend_down:
+                    self._notify_backend_down()
+                else:
+                    self._display_skill_loading_notification()
+                    self.bus.emit(Message('mycroft.internet.connected'))
+                    self._ensure_device_is_paired()
+                    self._update_device_attributes_on_backend()
 
     def _get_pairing_status(self):
         """Set an instance attribute indicating the device's pairing status"""
@@ -150,6 +93,9 @@ class DevicePrimer(object):
         except BackendDown:
             LOG.error('Cannot complete device updates due to backend issues.')
             self.backend_down = True
+
+        if self.is_paired:
+            LOG.info('Device is paired')
 
     def _update_system_clock(self):
         """Force a sync of the local clock with the Network Time Protocol.
@@ -160,55 +106,42 @@ class DevicePrimer(object):
         because it could have a negative impact on other software running on
         that device.
         """
-        LOG.info('Updating the system clock via NTP...')
         if self.platform in RASPBERRY_PI_PLATFORMS:
+            LOG.info('Updating the system clock via NTP...')
             if self.is_paired:
                 # Only display time sync message when paired because the prompt
                 # to go to home.mycroft.ai will be displayed by the pairing
                 # skill when pairing
                 self.enclosure.mouth_text(dialog.get("message_synching.clock"))
-            bus.wait_for_response(
+            self.bus.wait_for_response(
                 Message('system.ntp.sync'),
                 'system.ntp.sync.complete',
                 15
             )
 
-    def _check_time_skew(self):
+    def _reboot(self):
         """If the NTP sync skewed system time significantly, reboot.
 
         If system time moved by over an hour in the NTP sync, force a reboot to
         prevent weird things from occurring due to the 'time warp'.
         """
-        reboot = False
-        skew = abs((time.monotonic() - start_ticks) -
-                   (time.time() - start_clock))
-        if skew > ONE_HOUR:
-            LOG.warning(
-                'Clock sync altered system time by more than one hour,'
-                ' rebooting...'
-            )
-            data = {'utterance': dialog.get("time.changed.reboot")}
-            bus.emit(Message("speak", data))
-            wait_while_speaking()
-            # provide visual indicators of the reboot
-            self.enclosure.mouth_text(dialog.get("message_rebooting"))
-            self.enclosure.eyes_color(70, 65, 69)  # soft gray
-            self.enclosure.eyes_spin()
-            # give the system time to finish processing enclosure messages
-            time.sleep(1.0)
-            # reboot
-            bus.emit(Message("system.reboot"))
-            reboot = True
-        else:
-            bus.emit(Message("enclosure.mouth.reset"))
-            time.sleep(0.5)
-
-        return reboot
+        LOG.warning(
+            'Clock sync altered system time by more than one hour,'
+            ' rebooting...'
+        )
+        self._speak_dialog(dialog_id="time.changed.reboot", wait=True)
+        # provide visual indicators of the reboot
+        self.enclosure.mouth_text(dialog.get("message_rebooting"))
+        self.enclosure.eyes_color(70, 65, 69)  # soft gray
+        self.enclosure.eyes_spin()
+        # give the system time to finish processing enclosure messages
+        time.sleep(1.0)
+        # reboot
+        self.bus.emit(Message("system.reboot"))
 
     def _notify_backend_down(self):
         """Notify user of inability to communicate with the backend."""
-        data = {'utterance': dialog.get("backend.down")}
-        self.bus.emit(Message("speak", data))
+        self._speak_dialog(dialog_id="backend.down")
         self.bus.emit(Message("backend.down"))
 
     def _display_skill_loading_notification(self):
@@ -239,12 +172,11 @@ class DevicePrimer(object):
                 api = DeviceApi()
                 api.update_version()
             except BackendDown:
-                data = {'utterance': dialog.get("backend.down")}
-                bus.emit(Message("speak", data))
-                bus.emit(Message("backend.down"))
+                self._notify_backend_down()
 
-    def _attempt_system_update(self):
+    def _update_system(self):
         """Emit an update event that will be handled by the admin service."""
+        update_attempted = False
         if not self.is_paired:
             LOG.info('Attempting system update...')
             self.bus.emit(Message('system.update'))
@@ -260,54 +192,109 @@ class DevicePrimer(object):
                     'system.update.complete',
                     1000
                 )
+                update_attempted = True
+
+        return update_attempted
+
+    def _speak_dialog(self, dialog_id, wait=False):
+        data = {'utterance': dialog.get(dialog_id)}
+        self.bus.emit(Message("speak", data))
+        if wait:
+            wait_while_speaking()
 
 
 def main():
-    global bus
     reset_sigint_handler()
     # Create PID file, prevent multiple instances of this service
     Lock('skills')
     # Connect this Skill management process to the Mycroft message bus
-    bus = MessageBusClient()
-    bus.once('open', startup)
-    create_daemon(bus.run_forever)
+    config = Configuration.get()
+    bus = _start_message_bus_client(config)
+    _register_intent_services(bus)
+    event_scheduler = EventScheduler(bus)
+    skill_manager = _initialize_skill_manager(bus)
+    _wait_for_internet_connection()
+    if skill_manager is None:
+        skill_manager = _initialize_skill_manager(bus)
+    device_primer = DevicePrimer(bus, config)
+    device_primer.prepare_device()
+    skill_manager.start()
     wait_for_exit_signal()
-    shutdown()
+    shutdown(skill_manager, event_scheduler)
 
 
-def startup():
-    global bus, skill_service
-    config_manager = Configuration()
-    config_manager.set_config_update_handlers(bus)
-    config = config_manager.get()
+def _start_message_bus_client(config):
+    """Start the bus client daemon and wait for connection."""
+    bus = MessageBusClient()
+    Configuration.set_config_update_handlers(bus)
+    # Set the active lang to match the configured one
     set_active_lang(config.get('lang', 'en-us'))
-    skill_service = SkillService(bus, config)
-    skill_service.start()
-    _prepare_device(config)
-    LOG.info('Completed skill service startup!')
+    bus_connected = Event()
+    bus.on('message', create_echo_function('SKILLS'))
+    # Set the bus connected event when connection is established
+    bus.once('open', bus_connected.set)
+    create_daemon(bus.run_forever)
+
+    # Wait for connection
+    bus_connected.wait()
+    LOG.info('Connected to messagebus')
+
+    return bus
 
 
-def _prepare_device(config):
-    global bus
-    if connected():
-        device_primer = DevicePrimer(bus, config)
-        device_primer.prepare_device()
-    else:
-        thread = Timer(1, _prepare_device)
-        thread.daemon = True
-        thread.start()
+def _register_intent_services(bus):
+    """Start up the all intent services and connect them as needed.
+
+    Arguments:
+        bus: messagebus client to register the services on
+    """
+    service = IntentService(bus)
+    try:
+        PadatiousService(bus, service)
+    except Exception as e:
+        LOG.exception('Failed to create padatious handlers '
+                      '({})'.format(repr(e)))
+
+    # Register handler to trigger fallback system
+    bus.on('intent_failure', FallbackSkill.make_intent_failure_handler(bus))
 
 
-def shutdown():
+def _initialize_skill_manager(bus):
+    """Create a thread that monitors the loaded skills, looking for updates
+
+    Returns:
+        SkillManager instance
+    """
+    try:
+        skill_manager = SkillManager(bus)
+        skill_manager.load_priority()
+    except MsmException:
+        # skill manager couldn't be created, wait for network connection and
+        # retry
+        skill_manager = None
+        LOG.info(
+            'MSM is uninitialized and requires network connection to fetch '
+            'skill information\nWill retry after internet connection is '
+            'established.'
+        )
+
+    return skill_manager
+
+
+def _wait_for_internet_connection():
+    while not connected():
+        time.sleep(1)
+
+
+def shutdown(skill_manager, event_scheduler):
     LOG.info('Shutting down skill service')
-    global skill_service
-    if skill_service.event_scheduler is not None:
-        skill_service.event_scheduler.shutdown()
+    if event_scheduler is not None:
+        event_scheduler.shutdown()
     # Terminate all running threads that update skills
-    if skill_service.skill_manager is not None:
-        skill_service.skill_manager.stop()
-        skill_service.skill_manager.join()
-    LOG.info('Completed skill service shutdown!')
+    if skill_manager is not None:
+        skill_manager.stop()
+        skill_manager.join()
+    LOG.info('Skill service shutdown complete!')
 
 
 if __name__ == "__main__":
