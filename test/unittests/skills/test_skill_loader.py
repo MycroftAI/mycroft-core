e837a64745bd9cc8cf26f861521f9ef05e488ed0
Åke Forslund <ake.forslund@gmail.com>
2019-08-19 16:21:22 +0200

    Fix loader test

--- test/unittests/skills/test_skill_loader.py
+++ test/unittests/skills/test_skill_loader.py
@@ -1,6 +1,6 @@
 """Unit tests for the SkillLoader class."""
 from time import time
-from unittest.mock import call, Mock, patch
+from unittest.mock import call, MagicMock, patch
 
 from mycroft.skills.skill_loader import _get_last_modified_time, SkillLoader
 from ..base import MycroftUnitTestBase
@@ -20,10 +20,10 @@ class TestSkillLoader(MycroftUnitTestBase):
         )
         self._mock_skill_instance()
         # TODO: un-mock these when they are more testable
-        self.loader._load_skill_source = Mock(
-            return_value=Mock()
+        self.loader._load_skill_source = MagicMock(
+            return_value=MagicMock()
         )
-        self.loader._check_for_first_run = Mock()
+        self.loader._check_for_first_run = MagicMock()
 
     def _load_skill_directory(self):
         """The skill loader expects certain things in a skill directory."""
@@ -36,11 +36,11 @@ class TestSkillLoader(MycroftUnitTestBase):
 
     def _mock_skill_instance(self):
         """Mock the skill instance, we are not testing skill functionality."""
-        skill_instance = Mock()
+        skill_instance = MagicMock()
         skill_instance.name = 'test_skill'
         skill_instance.reload_skill = True
-        skill_instance.default_shutdown = Mock()
-        skill_instance.settings = Mock()
+        skill_instance.default_shutdown = MagicMock()
+        skill_instance.settings = MagicMock()
         self.skill_instance_mock = skill_instance
 
     def test_get_last_modified_date(self):
@@ -53,7 +53,7 @@ class TestSkillLoader(MycroftUnitTestBase):
 
     def test_skill_already_loaded(self):
         """The loader should take to action for an already loaded skill."""
-        self.loader.instance = Mock
+        self.loader.instance = MagicMock
         self.loader.instance.reload_skill = True
         self.loader.loaded = True
         self.loader.last_loaded = time() + ONE_MINUTE
@@ -63,7 +63,7 @@ class TestSkillLoader(MycroftUnitTestBase):
 
     def test_skill_reloading_blocked(self):
         """The loader should skip reloads for skill that doesn't allow it."""
-        self.loader.instance = Mock()
+        self.loader.instance = MagicMock()
         self.loader.instance.reload_skill = False
         self.loader.active = False
         self.loader.loaded = True
@@ -77,7 +77,7 @@ class TestSkillLoader(MycroftUnitTestBase):
 
     def test_skill_reload(self):
         """Test reloading a skill that was modified."""
-        self.loader.instance = Mock()
+        self.loader.instance = MagicMock()
         self.loader.loaded = True
         self.loader.last_loaded = 0
 
