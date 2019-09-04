3639fa3de1481f882e1b25188dae870deb2ca475
Kirill Kirilenko <kirill@ultracoder.org>
2019-09-04 23:50:59 +0300

    Add Yandex TTS support

--- mycroft/tts/__init__.py
+++ mycroft/tts/__init__.py
@@ -451,6 +451,7 @@ class TTSFactory:
     from mycroft.tts.ibm_tts import WatsonTTS
     from mycroft.tts.responsive_voice_tts import ResponsiveVoice
     from mycroft.tts.mimic2_tts import Mimic2
+    from mycroft.tts.yandex_tts import YandexTTS
 
     CLASSES = {
         "mimic": Mimic,
@@ -462,7 +463,8 @@ class TTSFactory:
         "spdsay": SpdSay,
         "watson": WatsonTTS,
         "bing": BingTTS,
-        "responsive_voice": ResponsiveVoice
+        "responsive_voice": ResponsiveVoice,
+        "yandex": YandexTTS
     }
 
     @staticmethod
