2cc90f3f9fee2fa9088534ee4bf5d952444e724f
Åke Forslund <ake.forslund@gmail.com>
2020-01-14 08:35:49 +0100

    Remove functions from format.py and parse.py

--- mycroft/util/format.py
+++ mycroft/util/format.py
@@ -19,6 +19,9 @@ things like numbers, times, etc.
 
 The focus of these formatting functions is to create natural sounding speech
 and allow localization.
+
+These functions have been extracted to lingua franca this file provides
+a compatible import of the main function of those files.
 """
 import datetime
 import warnings
@@ -26,10 +29,14 @@ import warnings
 from calendar import leapdays
 from enum import Enum
 
-from lingua_franca.format import (NUMBER_TUPLE, DateTimeFormat, join_list,
+from lingua_franca.format import (nice_time, nice_number, pronounce_number,
+                                  nice_date, nice_date_time, nice_year,
+
+                                  NUMBER_TUPLE, DateTimeFormat, join_list,
                                   date_time_format, expand_options,
                                   _translate_word)
 
+# TODO: Remove all imports from lingua_franca.lang in 20.02
 from lingua_franca.lang import get_full_lang_code, get_primary_lang_code
 
 from lingua_franca.lang.format_en import *
@@ -59,210 +66,6 @@ class TimeResolution(Enum):
     MINUTES = 4
     SECONDS = 5
     MILLISECONDS = 6
-
-
-def nice_number(number, lang=None, speech=True, denominators=None):
-    """Format a float to human readable functions
-
-    This function formats a float to human understandable functions. Like
-    4.5 becomes 4 and a half for speech and 4 1/2 for text
-    Args:
-        number (int or float): the float to format
-        lang (str): code for the language to use
-        speech (bool): format for speech (True) or display (False)
-        denominators (iter of ints): denominators to use, default [1 .. 20]
-    Returns:
-        (str): The formatted string.
-    """
-    # Convert to spoken representation in appropriate language
-    lang_code = get_primary_lang_code(lang)
-    if lang_code == "en":
-        return nice_number_en(number, speech, denominators)
-    elif lang_code == "es":
-        return nice_number_es(number, speech, denominators)
-    elif lang_code == "pt":
-        return nice_number_pt(number, speech, denominators)
-    elif lang_code == "it":
-        return nice_number_it(number, speech, denominators)
-    elif lang_code == "fr":
-        return nice_number_fr(number, speech, denominators)
-    elif lang_code == "sv":
-        return nice_number_sv(number, speech, denominators)
-    elif lang_code == "de":
-        return nice_number_de(number, speech, denominators)
-    elif lang_code == "hu":
-        return nice_number_hu(number, speech, denominators)
-    elif lang_code == "nl":
-        return nice_number_nl(number, speech, denominators)
-    elif lang_code == "da":
-        return nice_number_da(number, speech, denominators)
-    elif lang_code == "sv":
-        return nice_number_sv(number, speech, denominators)
-
-    # Default to the raw number for unsupported languages,
-    # hopefully the STT engine will pronounce understandably.
-    return str(number)
-
-
-def nice_time(dt, lang=None, speech=True, use_24hour=False,
-              use_ampm=False):
-    """
-    Format a time to a comfortable human format
-
-    For example, generate 'five thirty' for speech or '5:30' for
-    text display.
-
-    Args:
-        dt (datetime): date to format (assumes already in local timezone)
-        lang (str): code for the language to use
-        speech (bool): format for speech (default/True) or display (False)
-        use_24hour (bool): output in 24-hour/military or 12-hour format
-        use_ampm (bool): include the am/pm for 12-hour format
-    Returns:
-        (str): The formatted time string
-    """
-    lang_code = get_primary_lang_code(lang)
-    if lang_code == "en":
-        return nice_time_en(dt, speech, use_24hour, use_ampm)
-    elif lang_code == "es":
-        return nice_time_es(dt, speech, use_24hour, use_ampm)
-    elif lang_code == "it":
-        return nice_time_it(dt, speech, use_24hour, use_ampm)
-    elif lang_code == "fr":
-        return nice_time_fr(dt, speech, use_24hour, use_ampm)
-    elif lang_code == "de":
-        return nice_time_de(dt, speech, use_24hour, use_ampm)
-    elif lang_code == "hu":
-        return nice_time_hu(dt, speech, use_24hour, use_ampm)
-    elif lang_code == "nl":
-        return nice_time_nl(dt, speech, use_24hour, use_ampm)
-    elif lang_code == "da":
-        return nice_time_da(dt, speech, use_24hour, use_ampm)
-    elif lang_code == "pt":
-        return nice_time_pt(dt, speech, use_24hour, use_ampm)
-    elif lang_code == "sv":
-        return nice_time_sv(dt, speech, use_24hour, use_ampm)
-
-    # TODO: Other languages
-    return str(dt)
-
-
-def pronounce_number(number, lang=None, places=2, short_scale=True,
-                     scientific=False):
-    """
-    Convert a number to it's spoken equivalent
-
-    For example, '5' would be 'five'
-
-    Args:
-        number: the number to pronounce
-        short_scale (bool) : use short (True) or long scale (False)
-            https://en.wikipedia.org/wiki/Names_of_large_numbers
-        scientific (bool) : convert and pronounce in scientific notation
-    Returns:
-        (str): The pronounced number
-    """
-    lang_code = get_primary_lang_code(lang)
-    if lang_code == "en":
-        return pronounce_number_en(number, places=places,
-                                   short_scale=short_scale,
-                                   scientific=scientific)
-    elif lang_code == "it":
-        return pronounce_number_it(number, places=places,
-                                   short_scale=short_scale,
-                                   scientific=scientific)
-    elif lang_code == "es":
-        return pronounce_number_es(number, places=places)
-    elif lang_code == "fr":
-        return pronounce_number_fr(number, places=places)
-    elif lang_code == "de":
-        return pronounce_number_de(number, places=places)
-    elif lang_code == "hu":
-        return pronounce_number_hu(number, places=places)
-    elif lang_code == "nl":
-        return pronounce_number_nl(number, places=places)
-    elif lang_code == "da":
-        return pronounce_number_da(number, places=places)
-    elif lang_code == "pt":
-        return pronounce_number_pt(number, places=places)
-    elif lang_code == "sv":
-        return pronounce_number_sv(number, places=places)
-
-    # Default to just returning the numeric value
-    return str(number)
-
-
-def nice_date(dt, lang=None, now=None):
-    """
-    Format a datetime to a pronounceable date
-
-    For example, generates 'tuesday, june the fifth, 2018'
-    Args:
-        dt (datetime): date to format (assumes already in local timezone)
-        lang (string): the language to use, use Mycroft default language if not
-            provided
-        now (datetime): Current date. If provided, the returned date for speech
-            will be shortened accordingly: No year is returned if now is in the
-            same year as td, no month is returned if now is in the same month
-            as td. If now and td is the same day, 'today' is returned.
-    Returns:
-        (str): The formatted date string
-    """
-    full_code = get_full_lang_code(lang)
-    date_time_format.cache(full_code)
-
-    return date_time_format.date_format(dt, full_code, now)
-
-
-def nice_date_time(dt, lang=None, now=None, use_24hour=False,
-                   use_ampm=False):
-    """
-        Format a datetime to a pronounceable date and time
-
-        For example, generate 'tuesday, june the fifth, 2018 at five thirty'
-
-        Args:
-            dt (datetime): date to format (assumes already in local timezone)
-            lang (string): the language to use, use Mycroft default language if
-                not provided
-            now (datetime): Current date. If provided, the returned date for
-                speech will be shortened accordingly: No year is returned if
-                now is in the same year as td, no month is returned if now is
-                in the same month as td. If now and td is the same day, 'today'
-                is returned.
-            use_24hour (bool): output in 24-hour/military or 12-hour format
-            use_ampm (bool): include the am/pm for 12-hour format
-        Returns:
-            (str): The formatted date time string
-    """
-
-    full_code = get_full_lang_code(lang)
-    date_time_format.cache(full_code)
-
-    return date_time_format.date_time_format(dt, full_code, now, use_24hour,
-                                             use_ampm)
-
-
-def nice_year(dt, lang=None, bc=False):
-    """
-        Format a datetime to a pronounceable year
-
-        For example, generate 'nineteen-hundred and eighty-four' for year 1984
-
-        Args:
-            dt (datetime): date to format (assumes already in local timezone)
-            lang (string): the language to use, use Mycroft default language if
-            not provided
-            bc (bool) pust B.C. after the year (python does not support dates
-                B.C. in datetime)
-        Returns:
-            (str): The formatted year string
-    """
-
-    full_code = get_full_lang_code(lang)
-    date_time_format.cache(full_code)
-
-    return date_time_format.year_format(dt, full_code, bc)
 
 
 def _duration_handler(time1, lang=None, speech=True, *, time2=None,
--- mycroft/util/parse.py
+++ mycroft/util/parse.py
@@ -20,12 +20,18 @@ things like numbers, times, durations etc.
 
 The focus of these parsing functions is to extract data from natural speech
 and to allow localization.
+
+This file imports the main functions from lingua franca providing a
+compatibility layer.
 """
 
 from difflib import SequenceMatcher
-from mycroft.util.time import now_local
+
+from lingua_franca.parse import (extract_number, extract_datetime, normalize,
+                                 get_gender)
 from lingua_franca.lang import get_primary_lang_code
 
+# TODO: Remove all imports below from lingua_franca.lang in v20.02
 from lingua_franca.lang.parse_en import *
 from lingua_franca.lang.parse_pt import *
 from lingua_franca.lang.parse_es import *
@@ -44,10 +50,9 @@ from lingua_franca.lang.parse_da import extractnumber_da
 from lingua_franca.lang.parse_da import extract_numbers_da
 from lingua_franca.lang.parse_da import extract_datetime_da
 from lingua_franca.lang.parse_da import normalize_da
-from .lang.parse_nl import extractnumber_nl
-from .lang.parse_nl import extract_datetime_nl
-from .lang.parse_nl import normalize_nl
-
+from lingua_franca.lang.parse_nl import extractnumber_nl
+from lingua_franca.lang.parse_nl import extract_datetime_nl
+from lingua_franca.lang.parse_nl import normalize_nl
 
 from .log import LOG
 
@@ -137,49 +142,6 @@ def extract_numbers(text, short_scale=True, ordinals=False, lang=None):
     return []
 
 
-def extract_number(text, short_scale=True, ordinals=False, lang=None):
-    """Takes in a string and extracts a number.
-
-    Args:
-        text (str): the string to extract a number from
-        short_scale (bool): Use "short scale" or "long scale" for large
-            numbers -- over a million.  The default is short scale, which
-            is now common in most English speaking countries.
-            See https://en.wikipedia.org/wiki/Names_of_large_numbers
-        ordinals (bool): consider ordinal numbers, e.g. third=3 instead of 1/3
-        lang (str): the BCP-47 code for the language to use, None uses default
-    Returns:
-        (int, float or False): The number extracted or False if the input
-                               text contains no numbers
-    """
-    lang_code = get_primary_lang_code(lang)
-    if lang_code == "en":
-        return extractnumber_en(text, short_scale=short_scale,
-                                ordinals=ordinals)
-    elif lang_code == "es":
-        return extractnumber_es(text)
-    elif lang_code == "pt":
-        return extractnumber_pt(text)
-    elif lang_code == "it":
-        return extractnumber_it(text, short_scale=short_scale,
-                                ordinals=ordinals)
-    elif lang_code == "fr":
-        return extractnumber_fr(text)
-    elif lang_code == "sv":
-        return extractnumber_sv(text)
-    elif lang_code == "de":
-        return extractnumber_de(text)
-    elif lang_code == "da":
-        return extractnumber_da(text)
-    elif lang_code == "nl":
-        return extractnumber_nl(text, short_scale=short_scale,
-                                ordinals=ordinals)
-    # TODO: extractnumber_xx for other languages
-    _log_unsupported_language(lang_lower, ['en', 'es', 'pt', 'it', 'fr', 'sv',
-                                           'de', 'da', 'nl'])
-    return text
-
-
 def extract_duration(text, lang=None):
     """ Convert an english phrase into a number of seconds
 
@@ -213,153 +175,4 @@ def extract_duration(text, lang=None):
 
     # TODO: extract_duration for other languages
     _log_unsupported_language(lang_code, ['en'])
-    return None
-
-
-def extract_datetime(text, anchorDate=None, lang=None, default_time=None):
-    """
-    Extracts date and time information from a sentence.  Parses many of the
-    common ways that humans express dates and times, including relative dates
-    like "5 days from today", "tomorrow', and "Tuesday".
-
-    Vague terminology are given arbitrary values, like:
-        - morning = 8 AM
-        - afternoon = 3 PM
-        - evening = 7 PM
-
-    If a time isn't supplied or implied, the function defaults to 12 AM
-
-    Args:
-        text (str): the text to be interpreted
-        anchorDate (:obj:`datetime`, optional): the date to be used for
-            relative dating (for example, what does "tomorrow" mean?).
-            Defaults to the current local date/time.
-        lang (str): the BCP-47 code for the language to use, None uses default
-        default_time (datetime.time): time to use if none was found in
-            the input string.
-
-    Returns:
-        [:obj:`datetime`, :obj:`str`]: 'datetime' is the extracted date
-            as a datetime object in the user's local timezone.
-            'leftover_string' is the original phrase with all date and time
-            related keywords stripped out. See examples for further
-            clarification
-
-            Returns 'None' if no date or time related text is found.
-
-    Examples:
-
-        >>> extract_datetime(
-        ... "What is the weather like the day after tomorrow?",
-        ... datetime(2017, 06, 30, 00, 00)
-        ... )
-        [datetime.datetime(2017, 7, 2, 0, 0), 'what is weather like']
-
-        >>> extract_datetime(
-        ... "Set up an appointment 2 weeks from Sunday at 5 pm",
-        ... datetime(2016, 02, 19, 00, 00)
-        ... )
-        [datetime.datetime(2016, 3, 6, 17, 0), 'set up appointment']
-
-        >>> extract_datetime(
-        ... "Set up an appointment",
-        ... datetime(2016, 02, 19, 00, 00)
-        ... )
-        None
-    """
-
-    lang_code = get_primary_lang_code(lang)
-
-    if not anchorDate:
-        anchorDate = now_local()
-
-    if lang_code == "en":
-        return extract_datetime_en(text, anchorDate, default_time)
-    elif lang_code == "es":
-        return extract_datetime_es(text, anchorDate, default_time)
-    elif lang_code == "pt":
-        return extract_datetime_pt(text, anchorDate, default_time)
-    elif lang_code == "it":
-        return extract_datetime_it(text, anchorDate, default_time)
-    elif lang_code == "fr":
-        return extract_datetime_fr(text, anchorDate, default_time)
-    elif lang_code == "sv":
-        return extract_datetime_sv(text, anchorDate, default_time)
-    elif lang_code == "de":
-        return extract_datetime_de(text, anchorDate, default_time)
-    elif lang_code == "da":
-        return extract_datetime_da(text, anchorDate, default_time)
-    elif lang_code == "nl":
-        return extract_datetime_nl(text, anchorDate, default_time)
-    # TODO: extract_datetime for other languages
-    _log_unsupported_language(lang_code, ['en', 'es', 'pt', 'it', 'fr', 'sv',
-                                          'de', 'da', 'nl'])
-    return text
-
-
-def normalize(text, lang=None, remove_articles=True):
-    """Prepare a string for parsing
-
-    This function prepares the given text for parsing by making
-    numbers consistent, getting rid of contractions, etc.
-
-    Args:
-        text (str): the string to normalize
-        lang (str): the BCP-47 code for the language to use, None uses default
-        remove_articles (bool): whether to remove articles (like 'a', or
-                                'the'). True by default.
-
-    Returns:
-        (str): The normalized string.
-    """
-
-    lang_code = get_primary_lang_code(lang)
-
-    if lang_code == "en":
-        return normalize_en(text, remove_articles)
-    elif lang_code == "es":
-        return normalize_es(text, remove_articles)
-    elif lang_code == "pt":
-        return normalize_pt(text, remove_articles)
-    elif lang_code == "it":
-        return normalize_it(text, remove_articles)
-    elif lang_code == "fr":
-        return normalize_fr(text, remove_articles)
-    elif lang_code == "sv":
-        return normalize_sv(text, remove_articles)
-    elif lang_code == "de":
-        return normalize_de(text, remove_articles)
-    elif lang_code == "da":
-        return normalize_da(text, remove_articles)
-    elif lang_code == "nl":
-        return normalize_nl(text, remove_articles)
-    # TODO: Normalization for other languages
-    _log_unsupported_language(lang_code, ['en', 'es', 'pt', 'it', 'fr', 'sv',
-                                          'de', 'da', 'nl'])
-    return text
-
-
-def get_gender(word, context="", lang=None):
-    """ Guess the gender of a word
-
-    Some languages assign genders to specific words.  This method will attempt
-    to determine the gender, optionally using the provided context sentence.
-
-    Args:
-        word (str): The word to look up
-        context (str, optional): String containing word, for context
-        lang (str): the BCP-47 code for the language to use, None uses default
-
-    Returns:
-        str: The code "m" (male), "f" (female) or "n" (neutral) for the gender,
-             or None if unknown/or unused in the given language.
-    """
-
-    lang_code = get_primary_lang_code(lang)
-
-    if lang_code in ["pt", "es"]:
-        # spanish follows same rules
-        return get_gender_pt(word, context)
-    elif lang_code == "it":
-        return get_gender_it(word, context)
     return None
