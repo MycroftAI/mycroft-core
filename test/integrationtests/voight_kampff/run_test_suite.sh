0f860162e3ad54fa3404140a746cb82d4b127730
Åke Forslund <ake.forslund@gmail.com>
2020-03-11 07:49:08 +0100

    Update run_test_suite.sh
    - Pulseaudio is launched if on CI
    - remove hard-coding of start-mycroft.sh

--- test/integrationtests/voight_kampff/run_test_suite.sh
+++ test/integrationtests/voight_kampff/run_test_suite.sh
@@ -5,15 +5,21 @@
 # runtime. Assumes running within a Docker container where the PATH environment
 # variable has been set to include the virtual envionrment's bin directory
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Start pulseaudio if running in CI environment
+if [[ -v CI ]]; then
+    pulseaudio -D
+fi
 # Start all mycroft core services.
-/opt/mycroft/mycroft-core/start-mycroft.sh all
+${SCRIPT_DIR}/../../../start-mycroft.sh all
 # Run the integration test suite.  Results will be formatted for input into
 # the Allure reporting tool.
 echo "Running behave with the arguments \"$@\""
 behave $@
 RESULT=$?
 # Stop all mycroft core services.
-/opt/mycroft/mycroft-core/stop-mycroft.sh all
+${SCRIPT_DIR}/../../../stop-mycroft.sh all
 
 # Reort the result of the behave test as exit status
 exit $RESULT
