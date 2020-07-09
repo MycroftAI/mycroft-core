862aade5b84932360a41186afc1ed105ce73d4a4
NoamDev <37066741+NoamDev@users.noreply.github.com>
2020-07-09 15:07:57 +0300

    removed dependence on cgholke's builds and pipwin.

--- win_minimal_setup.ps1
+++ win_minimal_setup.ps1
@@ -3,9 +3,4 @@ py -3.6 -m venv .venv
 ./venv-activate
 
 echo "Installing dependencies"
-echo "...pipwin..."
-pip3 install pipwin==0.5.0
-echo "...fann2..."
-python -m pipwin install -r requirements\pipwin-requirements.txt
-echo "...requirements..."
 pip3 install -r requirements\win-requirements.txt
