fa0b0dbd4190e223f3a9cfb5d46682a2122a63a1
Kris Gesling <kris.gesling@mycroft.ai>
2020-07-31 16:27:06 +0930

    Change names for consistency

--- mycroft/skills/__main__.py
+++ mycroft/skills/__main__.py
@@ -198,7 +198,7 @@ def main(ready_hook=on_ready, error_hook=on_error, stopping_hook=on_stopping,
     bus = _start_message_bus_client()
     _register_intent_services(bus)
     event_scheduler = EventScheduler(bus)
-    callbacks = StatusCallbackMap(on_complete=ready_hook,
+    callbacks = StatusCallbackMap(on_ready=ready_hook,
                                   on_error=error_hook,
                                   on_stopping=stopping_hook)
     status = ProcessStatus('skills', bus, callbacks)
