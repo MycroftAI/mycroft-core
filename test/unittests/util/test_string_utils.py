666178c48300ffefde05b66c7b6431c9a7f8c819
Kris Gesling <kris.gesling@mycroft.ai>
2020-08-18 23:06:42 +0930

    remove unnecessary remove_last_slash method

--- test/unittests/util/test_string_utils.py
+++ test/unittests/util/test_string_utils.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from mycroft.util import camel_case_split, remove_last_slash
+from mycroft.util import camel_case_split
 
 
 class TestStringFunctions(TestCase):
@@ -7,12 +7,3 @@ class TestStringFunctions(TestCase):
         """Check that camel case string is split properly."""
         self.assertEqual(camel_case_split('MyCoolSkill'), 'My Cool Skill')
         self.assertEqual(camel_case_split('MyCOOLSkill'), 'My COOL Skill')
-
-    def test_remove_last_slash(self):
-        """Check that the last slash in an url is correctly removed."""
-        self.assertEqual(remove_last_slash('https://github.com/'),
-                         'https://github.com')
-        self.assertEqual(remove_last_slash('https://github.com/hello'),
-                         'https://github.com/hello')
-        self.assertEqual(remove_last_slash('https://github.com/hello/'),
-                         'https://github.com/hello')
