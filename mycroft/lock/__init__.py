17953220350efcd661f0af4202be5087b0fa64ed
Juri Calleri <juricalleri@gmail.com>
2020-10-15 15:13:17 +0200

    Update __init__.py
    trailing whitespace removed and double space after '.' dot restored

--- mycroft/lock/__init__.py
+++ mycroft/lock/__init__.py
@@ -133,9 +133,9 @@ class Lock:  # python 3+ 'class Lock'
     #    Delete the exiting file
     def exists(self):
         """
-        Check if the PID lock file exists. If it does then 
-        send a SIGKILL signal to the process defined by the value
-        in the lock file. Catch the keyboard interrupt exception to
+        Check if the PID lock file exists.  If it does
+        then send a SIGKILL signal to the process defined by the value
+        in the lock file.  Catch the keyboard interrupt exception to
         prevent propagation if stopped by use of Ctrl-C.
         """
         if not os.path.isfile(self.path):
