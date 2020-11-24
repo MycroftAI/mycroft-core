eb78d67ff7835001539814410c52b0b78b277f4b
Åke Forslund <ake.forslund@gmail.com>
2020-11-24 10:49:04 +0100

    Fix AdaptIntent without name.

--- mycroft/skills/intent_services/adapt_service.py
+++ mycroft/skills/intent_services/adapt_service.py
@@ -26,9 +26,11 @@ from .base import IntentMatch
 class AdaptIntent(IntentBuilder):
     """Wrapper for IntentBuilder setting a blank name.
 
-    This is mainly here for backwards compatibility, adapt now support
-    automatically named IntentBulders.
+    Arguments:
+        name (str): Optional name of intent
     """
+    def __init__(self, name=''):
+        super().__init__(name)
 
 
 def _strip_result(context_features):
