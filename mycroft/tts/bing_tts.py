3c43afc814d1846c88ed563dc61dfe0fae17486a
domcross <dkreutz@t-online.de>
2020-12-08 21:35:18 +0100

    Commit headline -- not longer than this line (50)
    This is the template for commits to mycroft-core and also an example of
    a commit message. To use this template, change the first line (think of
    it as a "headline" for the commit) and then edit this message with a
    longer description of the change.  To be nice, keep lines to 72
    characters or less (The first two lines of this template are exactly
    that length).  Also use imperative writing, e.g. "Fix broken code" or
    "Implement my new feature", not "Fixes the broken code" or
    "Implemented my new feature".
    
    Sections below can then be filled out and edited as appropriate.
    Unused sections can optionally be removed.
    
    Please fill this out carefully.  This moment is when the code is
    clearest in the mind of you, the foremost expert in this change.
    Please pause to think of side effects and impacts.
    
    ==== Fixed Issues ====
    NONE - replace with associated issue numbers, e.g. #123, #304
    
    ====  Tech Notes ====
    NONE - explain new algorithms in detail, tool changes, etc.
    
    ====  Documentation Notes ====
    NONE - description of a new feature or notes on behavior changes
    
    ==== Localization Notes ====
    NONE - point to new strings, language specific functions, etc.
    
    ==== Environment Notes ====
    NONE - new package requirements, new files being written to disk, etc.
    
    ==== Protocol Notes ====
    NONE - message types added or changed, new signals, APIs, etc.

--- mycroft/tts/bing_tts.py
+++ mycroft/tts/bing_tts.py
@@ -13,25 +13,66 @@
 # limitations under the License.
 #
 
+import requests
 from .tts import TTS, TTSValidator
 from mycroft.configuration import Configuration
+from time import time
+from mycroft.util.log import LOG
 
 
 class BingTTS(TTS):
     def __init__(self, lang, config):
         super(BingTTS, self).__init__(lang, config, BingTTSValidator(self))
         self.type = 'wav'
-        from bingtts import Translator
         self.config = Configuration.get().get("tts", {}).get("bing", {})
-        api = self.config.get("api_key")
-        self.bing = Translator(api)
-        self.gender = self.config.get("gender", "Male")
-        self.format = self.config.get("format", "riff-16khz-16bit-mono-pcm")
+        self.subscription_key = self.config.get("api_key", "")
+        self.service_region = self.config.get("service_region", "centralus")
+        self.voice = self.config.get("voice_name", "en-US-JennyNeural")
+        self.format = self.config.get("format", "riff-24khz-16bit-mono-pcm")
+        self.token = None
+        self.token_ttl = 0
+
+    def _get_token(self):
+        if self.token and int(time()) < self.token_ttl:
+            LOG.debug("BingTTS token is still alive")
+            return self.token
+
+        fetch_token_url = "https://{}.api.cognitive.microsoft.com/sts/v1.0/issueToken".format(self.service_region)
+        headers = {
+            'Ocp-Apim-Subscription-Key': self.subscription_key
+        }
+        response = requests.post(fetch_token_url, headers=headers)
+        if response.status_code == 200:
+            self.token = str(response.text)
+            self.token_ttl = int(time()) + (9 * 60)
+            LOG.debug("BingTTS get token success - new ttl: {}".format(self.token_ttl))
+        else:
+            self.token = None
+            self.token_ttl = 0
+            LOG.error("BingTTS request token failed with error code: {}".format(response.status_code))
+        return self.token
 
     def get_tts(self, sentence, wav_file):
-        output = self.bing.speak(sentence, self.lang, self.gender,
-                                 self.format)
-        with open(wav_file, "w") as f:
+        tts_service_url = "https://{}.tts.speech.microsoft.com/cognitiveservices/v1".format(self.service_region)
+        headers = {
+            "Authorization": "Bearer {}".format(self._get_token()),
+            "X-Microsoft-OutputFormat": self.format,
+            "Content-Type": "application/ssml+xml",
+            "User-Agent": "mycroft.ai open-source voice assistant"
+        }
+        lang = self.voice[0:4]
+        xml = """
+            <speak version='1.0' xml:lang='{}'><voice name='{}'>
+            {}
+            </voice></speak>
+        """.format(lang, self.voice, sentence)
+        response = requests.post(tts_service_url, data=xml.encode("utf-8"), headers=headers)
+        if response.status_code != 200:
+            LOG.error("BingTTS speech request failed with error code {}".format(response.status_code))
+            return (None, None)
+        LOG.debug("BingTTS speech request success: {}".format(response.status_code))
+        output = response.content
+        with open(wav_file, "wb") as f:
             f.write(output)
         return (wav_file, None)  # No phonemes
 
@@ -41,20 +82,21 @@ class BingTTSValidator(TTSValidator):
         super(BingTTSValidator, self).__init__(tts)
 
     def validate_dependencies(self):
-        try:
-            from bingtts import Translator
-        except ImportError:
-            raise Exception(
-                'BingTTS dependencies not installed, please run pip install '
-                'git+https://github.com/westparkcom/Python-Bing-TTS.git ')
+        # nothing todo
+        pass
 
     def validate_lang(self):
         # TODO
         pass
 
     def validate_connection(self):
-        # TODO
-        pass
+        try:
+            if not self.tts._get_token():
+                raise Exception("BingTTS access token could not be retrieved. Please check your credentials")
+        except TypeError:
+            raise Exception(
+                'BingTTS server could not be verified. Please check your '
+                'internet connection.')
 
     def get_tts_class(self):
         return BingTTS
