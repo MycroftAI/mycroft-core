998475cb4be543f6aefa2942880d8e7cad66d1f6
Kris Gesling <kris.gesling@mycroft.ai>
2020-12-10 14:39:42 +0930

    Switch GPIO pins based on board type

--- mycroft/enclosure/hardware/MycroftSwitch/switch_gpio.py
+++ mycroft/enclosure/hardware/MycroftSwitch/switch_gpio.py
@@ -15,11 +15,12 @@
 import RPi.GPIO as GPIO
 import time
 from mycroft.util.log import LOG
- 
+
+
 class Switch:
     """
-    Class to handle the Mark2 switches.                                       x 
-    Note - a switch is an abstract concept 
+    Class to handle the Mark2 switches.                                       x
+    Note - a switch is an abstract concept
     which applies to buttons and switches.
     The Mark2 actually has 4 different switches.
     Three buttons (volume up, down and activate)
@@ -27,36 +28,39 @@ class Switch:
     and interrupt driven. Also note switches are
     pulled up so the active state is actually zero.
     """
-    # GPIO pin numbers
-    """ old sj201 mappings 
-    _SW_ACTION = 22
-    _SW_VOL_UP = 23
-    _SW_VOL_DOWN = 24 
-    _SW_MUTE = 25
-    """
-    # sj201Rev4
-    _SW_VOL_DOWN = 22
-    _SW_VOL_UP = 23
-    _SW_ACTION = 24
-    _SW_MUTE = 25
-
-    _XMOS_POWER = 16  # Enable1V
-    _XMOS_RESET = 27  # Reset XMOS
 
     def __init__(self, debounce=100):
         self.debounce = debounce
         self.active = 0
+
+        config = Configuration.get()
+        self.board_type = config["enclosure"].get("board_type", "sj201r4")
+        # GPIO pin numbers
+        if self.board_type == "sj201r4":
+            self._SW_VOL_DOWN = 22
+            self._SW_VOL_UP = 23
+            self._SW_ACTION = 24
+            self._SW_MUTE = 25
+            self._XMOS_POWER = 16  # Enable1V
+            self._XMOS_RESET = 27  # Reset XMOS
+        else:
+            self._SW_ACTION = 22
+            self._SW_VOL_UP = 23
+            self._SW_VOL_DOWN = 24
+            self._SW_MUTE = 25
+            self._XMOS_POWER = 13
+            self._XMOS_RESET = None  # Not used
 
         # some switch implementations require a thread
         # we don't but we must meet the base requirement
         self.thread_handle = None
 
         self.capabilities = {
-                "user_volup_handler":"button",
-                "user_voldown_handler":"button",
-                "user_action_handler":"button",
-                "user_mute_handler":"slider"
-                }
+            "user_volup_handler": "button",
+            "user_voldown_handler": "button",
+            "user_action_handler": "button",
+            "user_mute_handler": "slider",
+        }
 
         # use BCM GPIO pin numbering
         GPIO.setmode(GPIO.BCM)
@@ -66,16 +70,16 @@ class Switch:
         # xmos related
         GPIO.setup(self._XMOS_POWER, GPIO.OUT)
         GPIO.setup(self._XMOS_RESET, GPIO.OUT)
-
-        # power up the xmos 
-        self.reset_xmos()
         """
+        if self.board_type == "sj201r3":
+            # power up the xmos
+            self.reset_xmos()
 
         # we need to pull up the 3 buttons
         GPIO.setup(self._SW_ACTION, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self._SW_VOL_UP, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(self._SW_VOL_DOWN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-        GPIO.setup(self._SW_MUTE, GPIO.IN)   # not sure about the slider
+        GPIO.setup(self._SW_MUTE, GPIO.IN)  # not sure about the slider
 
         # establish default values
         self.SW_ACTION = GPIO.input(self._SW_ACTION)
@@ -83,32 +87,37 @@ class Switch:
         self.SW_VOL_DOWN = GPIO.input(self._SW_VOL_DOWN)
         self.SW_MUTE = GPIO.input(self._SW_MUTE)
 
-        # establish default handlers for each switch 
+        # establish default handlers for each switch
         self.action_handler = self.handle_action
         self.vol_up_handler = self.handle_vol_up
         self.vol_down_handler = self.handle_vol_down
         self.mute_handler = self.handle_mute
 
         # attach callbacks
-        GPIO.add_event_detect(self._SW_ACTION, 
-            GPIO.BOTH, 
-            callback=self.action_handler, 
-            bouncetime=debounce)
+        GPIO.add_event_detect(
+            self._SW_ACTION,
+            GPIO.BOTH,
+            callback=self.action_handler,
+            bouncetime=debounce,
+        )
 
-        GPIO.add_event_detect(self._SW_VOL_UP, 
-            GPIO.BOTH, 
-            callback=self.vol_up_handler, 
-            bouncetime=debounce)
+        GPIO.add_event_detect(
+            self._SW_VOL_UP,
+            GPIO.BOTH,
+            callback=self.vol_up_handler,
+            bouncetime=debounce,
+        )
 
-        GPIO.add_event_detect(self._SW_VOL_DOWN, 
-            GPIO.BOTH, 
-            callback=self.vol_down_handler, 
-            bouncetime=debounce)
+        GPIO.add_event_detect(
+            self._SW_VOL_DOWN,
+            GPIO.BOTH,
+            callback=self.vol_down_handler,
+            bouncetime=debounce,
+        )
 
-        GPIO.add_event_detect(self._SW_MUTE, 
-            GPIO.BOTH, 
-            callback=self.mute_handler, 
-            bouncetime=debounce)
+        GPIO.add_event_detect(
+            self._SW_MUTE, GPIO.BOTH, callback=self.mute_handler, bouncetime=debounce
+        )
 
         # user overides
         self.user_voldown_handler = None
@@ -142,15 +151,20 @@ class Switch:
         if self.user_mute_handler is not None:
             self.user_mute_handler(self.SW_MUTE)
 
-    # recycle xmos power
     def reset_xmos(self):
-        GPIO.output(self._XMOS_RESET, 0)
-        time.sleep(0.001)
-        GPIO.output(self._XMOS_POWER, 0)
-        time.sleep(0.001)
-        GPIO.output(self._XMOS_POWER, 1)
-        time.sleep(0.001)
-        GPIO.output(self._XMOS_RESET, 1)
+        """Recycle xmos power"""
+        if self.board_type == "sj201r4":
+            GPIO.output(self._XMOS_RESET, 0)
+            time.sleep(0.001)
+            GPIO.output(self._XMOS_POWER, 0)
+            time.sleep(0.001)
+            GPIO.output(self._XMOS_POWER, 1)
+            time.sleep(0.001)
+            GPIO.output(self._XMOS_RESET, 1)
+        elif self.board_type == "sj201r3":
+            GPIO.output(self._XMOS_POWER, 0)
+            time.sleep(2.50)
+            GPIO.output(self._XMOS_POWER, 1)
 
     def terminate(self):
         LOG.debug("switch_gpio: terminate hit, calling GPIO.cleanup()")
