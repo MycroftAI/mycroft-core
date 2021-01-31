844e8990408199c28bef5fb6b97a0eee57ec6f11
NoamDev <37066741+NoamDev@users.noreply.github.com>
2021-01-31 21:28:17 +0200

    replaced /tmp with cross platform equivalent

--- mycroft/identity/__init__.py
+++ mycroft/identity/__init__.py
@@ -15,11 +15,13 @@
 import json
 import time
 import os
+from os.path import join
+import tempfile
 
 from mycroft.filesystem import FileSystemAccess
 from mycroft.util.log import LOG
 from mycroft.util.combo_lock import ComboLock
-identity_lock = ComboLock('/tmp/identity-lock')
+identity_lock = ComboLock(join(tempfile.gettempdir(),'identity-lock')
 
 
 class DeviceIdentity:
