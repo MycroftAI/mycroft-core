01c115e3ccd442bfe07f2d140da895d7e5e40a75
NoamDev <37066741+NoamDev@users.noreply.github.com>
2021-01-31 21:27:39 +0200

    some fixes to win setup

--- win_minimal_setup.bat
+++ win_minimal_setup.bat
@@ -12,9 +12,8 @@ python scripts\install-mimic-win.py || exit /b 1
 echo "Building precise"
 git clone https://github.com/NoamDev/mycroft-precise.git precise-engine-build || exit /b 1
 cd precise-engine-build
-git checkout 9c2eca94459a93dff24f36013d87b4923ee31c75 || exit /b 1
-./build.bat || exit /b 1
+git -c advice.detachedHead=false checkout 9c2eca94459a93dff24f36013d87b4923ee31c75 || exit /b 1
+.\build.bat || exit /b 1
 cd ..
-python -c "import shutil;from os.path import expanduser;from glob import glob;precise_targz = glob('precise-engine-build/dist/precise-engine*.tar.gz')[0];shutil.unpack_archive(precise_targz',expanduser('~/.mycroft/precise'))" || exit /b 1
-del /q precise-engine-build
-rmdir precise-engine-build
+python -c "import shutil;from os.path import expanduser;from glob import glob;precise_targz = glob('precise-engine-build/dist/precise-engine*.tar.gz')[0];shutil.unpack_archive(precise_targz,expanduser('~/.mycroft/precise'))" || exit /b 1
+rmdir /s /q precise-engine-build
