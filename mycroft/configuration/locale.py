0b4c78d309de7f6ed6002f4b553f9d0baa72cff3
Kris Gesling <kris.gesling@mycroft.ai>
2021-05-08 07:34:48 +0930

    Add warning of future method change

--- mycroft/configuration/locale.py
+++ mycroft/configuration/locale.py
@@ -25,6 +25,10 @@ from lingua_franca import set_default_lang as _set_default_lf_lang
 def set_default_lf_lang(lang_code="en-us"):
     """Set the default language of Lingua Franca for parsing and formatting.
 
+    Note: this is a temporary method until a global set_default_lang() method
+    can be implemented that updates all Mycroft systems eg STT and TTS.
+    It will be deprecated at the earliest possible point.
+
     Args:
         lang (str): BCP-47 language code, e.g. "en-us" or "es-mx"
     """
