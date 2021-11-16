c06f6aec8b0fef5eddfc99586f75f1ae5632623c
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2021-11-16 23:45:15 +0000

    fix/RemoteTTS+remove_requests_futures_dep (#14)
    RemoteTTS is unused by core, it was not ported to OPM and is only present for backwards compat imports in mycroft plugins that do not use OPM
    
    The sentence chunking has not been updated for a long time, the filename is not respected properly, it only supports .wav ....
    
    requests futures was only used by RemoteTTS, dated from 2016, it has been removed from dependencies
    
    This PR simplifies RemoteTTS to depend only on requests and no longer overrides the execute method, it behaves like any regular TTS now.
    
    Bugs are fixed at the cost of no automatic sentence chunking, text size limits should be handled in individual plugins
    
    authored-by: jarbasai <jarbasai@mailfence.com>

--- mycroft/tts/remote_tts.py
+++ mycroft/tts/remote_tts.py
@@ -13,11 +13,8 @@
 # limitations under the License.
 #
 import abc
-import re
-from requests_futures.sessions import FuturesSession
-
+import requests
 from mycroft.tts.tts import TTS
-from mycroft.util import play_wav
 from mycroft.util.log import LOG
 
 
@@ -32,63 +29,25 @@ class RemoteTTSTimeoutException(RemoteTTSException):
 class RemoteTTS(TTS):
     """
     Abstract class for a Remote TTS engine implementation.
-
-    It provides a common logic to perform multiple requests by splitting the
-    whole sentence into small ones.
+    This class is only provided as import for mycroft plugins that do not use OPM
+    Usage is discouraged
     """
-
     def __init__(self, lang, config, url, api_path, validator):
         super(RemoteTTS, self).__init__(lang, config, validator)
         self.api_path = api_path
         self.auth = None
         self.url = config.get('url', url).rstrip('/')
-        self.session = FuturesSession()
-
-    def execute(self, sentence, ident=None, listen=False):
-        phrases = self.__get_phrases(sentence)
-
-        if len(phrases) > 0:
-            for req in self.__requests(phrases):
-                try:
-                    self.begin_audio()
-                    self.__play(req)
-                except Exception as e:
-                    LOG.error(e.message)
-                finally:
-                    self.end_audio(listen)
-
-    @staticmethod
-    def __get_phrases(sentence):
-        phrases = re.split(r'\.+[\s+|\n]', sentence)
-        phrases = [p.replace('\n', '').strip() for p in phrases]
-        phrases = [p for p in phrases if len(p) > 0]
-        return phrases
-
-    def __requests(self, phrases):
-        reqs = []
-        for p in phrases:
-            reqs.append(self.__request(p))
-        return reqs
-
-    def __request(self, p):
-        return self.session.get(
-            self.url + self.api_path, params=self.build_request_params(p),
-            timeout=10, verify=False, auth=self.auth)
 
     @abc.abstractmethod
     def build_request_params(self, sentence):
         pass
 
-    def __play(self, req):
-        resp = req.result()
-        if resp.status_code == 200:
-            self.__save(resp.content)
-            play_wav(self.filename).communicate()
-        else:
-            LOG.error(
-                '%s Http Error: %s for url: %s' %
-                (resp.status_code, resp.reason, resp.url))
-
-    def __save(self, data):
-        with open(self.filename, 'wb') as f:
-            f.write(data)
+    def get_tts(self, sentence, wav_file, lang=None):
+        r = requests.get(
+            self.url + self.api_path, params=self.build_request_params(sentence),
+            timeout=10, verify=False, auth=self.auth)
+        if r.status_code != 200:
+            return None
+        with open(wav_file, 'wb') as f:
+            f.write(r.content)
+        return wav_file, None
