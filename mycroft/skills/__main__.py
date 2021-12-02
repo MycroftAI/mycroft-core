712110e17f422fda4df9ab28063fc70fa04e9b14
Chris Veilleux <veilleux.chris@gmail.com>
2021-12-02 16:01:00 -0600

    Removed DevicePrimer class.  All functionality moved to enclosure

--- mycroft/skills/__main__.py
+++ mycroft/skills/__main__.py
@@ -19,16 +19,10 @@ directory.  The executable gets added to the bin directory when installed
 (see setup.py)
 """
 import time
-from threading import Event
 
-from msm.exceptions import MsmException
 from lingua_franca import load_languages
 
 import mycroft.lock
-from mycroft import dialog
-from mycroft.api import is_paired, BackendDown, DeviceApi
-from mycroft.audio import wait_while_speaking
-from mycroft.enclosure.api import EnclosureAPI
 from mycroft.configuration import Configuration
 from mycroft.messagebus.message import Message
 from mycroft.util import (
@@ -44,132 +38,6 @@ from .core import FallbackSkill
 from .event_scheduler import EventScheduler
 from .intent_service import IntentService
 from .skill_manager import SkillManager
-
-RASPBERRY_PI_PLATFORMS = ('mycroft_mark_1', 'picroft', 'mycroft_mark_2')
-
-
-class DevicePrimer(object):
-    """Container handling the device preparation.
-
-    Args:
-        message_bus_client: Bus client used to interact with the system
-    """
-    def __init__(self, message_bus_client):
-        self.bus = message_bus_client
-        config = Configuration.get()
-        self.platform = config['enclosure'].get("platform", "unknown")
-        self.enclosure = EnclosureAPI(self.bus)
-        self.is_paired = False
-        self.backend_down = False
-        # Remember "now" at startup.  Used to detect clock changes.
-
-    def prepare_device(self):
-        """Internet dependent updates of various aspects of the device."""
-        self._get_pairing_status()
-        self._update_system_clock()
-        self._update_system()
-        # Above will block during update process and kill this instance if
-        # new software is installed
-
-        if self.backend_down:
-            self._notify_backend_down()
-        else:
-            self._display_skill_loading_notification()
-            self.bus.emit(Message('mycroft.internet.connected'))
-            self._ensure_device_is_paired()
-            self._update_device_attributes_on_backend()
-
-    def _get_pairing_status(self):
-        """Set an instance attribute indicating the device's pairing status"""
-        try:
-            self.is_paired = is_paired(ignore_errors=False)
-        except BackendDown:
-            LOG.error('Cannot complete device updates due to backend issues.')
-            self.backend_down = True
-
-        if self.is_paired:
-            LOG.info('Device is paired')
-
-    def _update_system_clock(self):
-        """Force a sync of the local clock with the Network Time Protocol.
-
-        The NTP sync is only forced on Raspberry Pi based devices.  The
-        assumption being that these devices are only running Mycroft services.
-        We don't want to sync the time on a Linux desktop device, for example,
-        because it could have a negative impact on other software running on
-        that device.
-        """
-        if self.platform in RASPBERRY_PI_PLATFORMS:
-            LOG.info('Updating the system clock via NTP...')
-            if self.is_paired:
-                # Only display time sync message when paired because the prompt
-                # to go to home.mycroft.ai will be displayed by the pairing
-                # skill when pairing
-                self.enclosure.mouth_text(dialog.get("message_synching.clock"))
-            self.bus.wait_for_response(
-                Message('system.ntp.sync'),
-                'system.ntp.sync.complete',
-                15
-            )
-
-    def _notify_backend_down(self):
-        """Notify user of inability to communicate with the backend."""
-        self._speak_dialog(dialog_id="backend.down")
-        self.bus.emit(Message("backend.down"))
-
-    def _display_skill_loading_notification(self):
-        """Indicate to the user that skills are being loaded."""
-        self.enclosure.eyes_color(189, 183, 107)  # dark khaki
-        self.enclosure.mouth_text(dialog.get("message_loading.skills"))
-
-    def _ensure_device_is_paired(self):
-        """Determine if device is paired, if not automatically start pairing.
-
-        Pairing cannot be performed if there is no connection to the back end.
-        So skip pairing if the backend is down.
-        """
-        if not self.is_paired and not self.backend_down:
-            LOG.info('Device not paired, invoking the pairing skill')
-            payload = dict(utterances=["pair my device"], lang="en-us")
-            self.bus.emit(Message("recognizer_loop:utterance", payload))
-
-    def _update_device_attributes_on_backend(self):
-        """Communicate version information to the backend.
-
-        The backend tracks core version, enclosure version, platform build
-        and platform name for each device, if it is known.
-        """
-        if self.is_paired:
-            LOG.info('Sending updated device attributes to the backend...')
-            try:
-                api = DeviceApi()
-                api.update_version()
-            except Exception:
-                self._notify_backend_down()
-
-    def _update_system(self):
-        """Emit an update event that will be handled by the admin service."""
-        if not self.is_paired:
-            LOG.info('Attempting system update...')
-            self.bus.emit(Message('system.update'))
-            msg = Message(
-                'system.update',
-                dict(paired=self.is_paired, platform=self.platform)
-            )
-            resp = self.bus.wait_for_response(msg, 'system.update.processing')
-
-            if resp and (resp.data or {}).get('processing', True):
-                self.bus.wait_for_response(
-                    Message('system.update.waiting'),
-                    'system.update.complete',
-                    1000
-                )
-
-    def _speak_dialog(self, dialog_id, wait=False):
-        data = {'utterance': dialog.get(dialog_id)}
-        self.bus.emit(Message("speak", data))
-        if wait:
-            wait_while_speaking()
 
 
 def on_started():
@@ -211,8 +79,6 @@ def main(alive_hook=on_alive, started_hook=on_started, ready_hook=on_ready,
     SkillApi.connect_bus(bus)
 
     _wait_for_internet_connection()
-    # device_primer = DevicePrimer(bus)
-    # device_primer.prepare_device()
     skill_manager = SkillManager(bus, watchdog)
     skill_manager.load_on_startup()
 
