1990d726a27d77c22afcd00939f1192b00959cbe
Chris Veilleux <veilleux.chris@gmail.com>
2021-12-22 15:49:59 -0600

    improved documentation

--- mycroft/skills/idle_display_skill.py
+++ mycroft/skills/idle_display_skill.py
@@ -1,4 +1,28 @@
-"""Provide a common API for skills that define idle screen functionality."""
+# Copyright 2021 Mycroft AI Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Provide a common API for skills that define idle screen functionality.
+
+The idle display should show when no other skill is using the display.  Some
+skills use the display for a defined period of time before returning to the
+idle display (e.g. Weather Skill).  Some skills take control of the display
+indefinitely (e.g. Timer Skill).
+
+The display could be a touch screen (such as on the Mark II), or an
+Arduino LED array (such as on the Mark I), or any other type of display.  This
+base class is meant to be agnostic to the type of display, with the
+implementation details defined within the skill that uses this as a base class.
+"""
 from datetime import datetime, timedelta
 
 from mycroft.messagebus import Message
@@ -71,8 +95,16 @@ class IdleDisplaySkill(MycroftSkill):
                     name="ShowIdle"
                 )
 
-    def _determine_expiration_time(self, message):
-        """Determine the time the page showing on the screen expires."""
+    def _determine_expiration_time(self, message) -> datetime:
+        """Determine the time the page showing on the screen expires.
+
+        Args:
+            message: Message sent over the bus to show a page on the display
+
+        Returns:
+            The date and time that the skill's GUI expires and should be
+            replaced with the idle screen.
+        """
         page_duration = None
         expiration_time = None
         if type(message.data["__idle"]) == int:
