47335a71764500f94f504b00147c2f047bd40760
Michael Hansen <mike@rhasspy.org>
2022-01-03 16:53:13 -0500

    Stop skill manager from updating skills

--- mycroft/skills/skill_manager.py
+++ mycroft/skills/skill_manager.py
@@ -214,22 +214,22 @@ class SkillManager(Thread):
         self._remove_git_locks()
 
         # Sync backend and skills.
-        if is_paired() and not self.upload_queue.started:
-            self._start_settings_update()
+        # if is_paired() and not self.upload_queue.started:
+        #     self._start_settings_update()
 
         # Scan the file folder that contains Skills.  If a Skill is updated,
         # unload the existing version from memory and reload from the disk.
         while not self._stop_event.is_set():
             try:
-                self._unload_removed_skills()
-                self._reload_modified_skills()
+                # self._unload_removed_skills()
+                # self._reload_modified_skills()
                 self._load_new_skills()
-                self._update_skills()
-                if (is_paired() and self.upload_queue.started and
-                        len(self.upload_queue) > 0):
-                    self.msm.clear_cache()
-                    self.skill_updater.post_manifest()
-                    self.upload_queue.send()
+                # self._update_skills()
+                # if (is_paired() and self.upload_queue.started and
+                #         len(self.upload_queue) > 0):
+                #     self.msm.clear_cache()
+                #     self.skill_updater.post_manifest()
+                #     self.upload_queue.send()
 
                 self._watchdog()
                 sleep(2)  # Pause briefly before beginning next scan
