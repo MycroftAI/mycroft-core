1622094ff7b6c1611eef79448660e245782869b4
Guy Martin <gmsoft@tuxicoman.be>
2022-09-30 10:11:22 +0200

    Replace / by _ in name to avoid issue when writing the cache file.

--- mycroft/skills/mycroft_skill/mycroft_skill.py
+++ mycroft/skills/mycroft_skill/mycroft_skill.py
@@ -1046,7 +1046,7 @@ class MycroftSkill:
         if not filename:
             raise FileNotFoundError('Unable to find "{}"'.format(entity_file))
 
-        name = '{}:{}'.format(self.skill_id, entity_file)
+        name = '{}:{}'.format(self.skill_id, entity_file.replace("/", "_"))
         self.intent_service.register_padatious_entity(name, filename)
 
     def handle_enable_intent(self, message):
